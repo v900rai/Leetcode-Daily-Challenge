0144ba54ff6a4ad26a8b43c9b4cfb401498e89f6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-10-27 21:05:05 +0530

    Time: 2 ms (84.49%), Space: 46 MB (20.88%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left=0;
-        for(int right=0; right<nums.length; right++){
-            if(nums[right]!=0){
-                int temp=nums[right];
-                nums[right]=nums[left];
-                nums[left]=temp;
-                left++;
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                if (left != right) {  // Only swap if left and right are different
+                    nums[left] = nums[right];
+                    nums[right] = 0; // Set right to zero since we moved the non-zero element
+                }
+                left++; // Move the left pointer
             }
         }
     }
