9b17f698d8d822a909086f296d285a4dfb1feaf8
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-11-05 12:27:43 +0530

    Time: 0 ms (100%), Space: 41.6 MB (31.6%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        // If the lengths are different, they can't be rotations of each other
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        
+        // Concatenate s with itself and check if goal is a substring
+        String doubled = s + s;
+        return doubled.contains(goal);
+    }
+}
