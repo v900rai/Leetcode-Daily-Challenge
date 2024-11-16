1822e66db364ea47c382915d13d1826a9e014023
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-11-16 12:36:09 +0530

    Time: 0 ms (100%), Space: 41.3 MB (98.16%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] plusOne(int[] digit) {
+        for(int i=digit.length-1; i>=0; i--){
+            if(digit[i]<9){
+                digit[i]++;
+                return digit;
+            }
+            digit[i]=0;
+        }
+        digit=new int [digit.length+1];
+        digit[0]=1;
+        return digit;
+        
+    }
+}
