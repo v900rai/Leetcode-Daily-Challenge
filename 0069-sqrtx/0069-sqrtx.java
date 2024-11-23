104980f40daf38dc36df99662034d87ec708190a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-11-23 15:00:58 +0530

    Time: 1 ms (86.54%), Space: 40.9 MB (32.93%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0 || x==1){
+            return x;
+        }
+        // initialize the search rage for the squre 
+        int start=0;
+        int end=x;
+        int mid=0;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            // if squre the middle value is greater then x
+            if(mid*mid==x){
+                return mid;
+
+            }
+            else if((long) mid *mid <x){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+            
+        }
+         return Math.round(end);
+        
+    }
+}
