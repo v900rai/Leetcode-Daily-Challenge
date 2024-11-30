b866c0e294fd36f372f651259d74945d7f8783de
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-11-30 21:52:16 +0530

    Time: 0 ms (100%), Space: 44.6 MB (21.68%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,25 +1,38 @@
 class Solution {
     
     public int removeDuplicates(int[] nums) {
-        if(nums.length == 1) return 1;
+        // if(nums.length == 1) return 1;
 
-        int i = 0;
-        int count = 1;
-        for(int j=i+1; j<nums.length; j++){
-            if(count<2 && nums[i]==nums[j]){
-                i++;
-                count++;
-                nums[i] = nums[j];
-            }else if(nums[i] !=nums[j]){
-                i++;
-                count=1;
-                nums[i] = nums[j];
+        // int i = 0;
+        // int count = 1;
+        // for(int j=i+1; j<nums.length; j++){
+        //     if(count<2 && nums[i]==nums[j]){
+        //         i++;
+        //         count++;
+        //         nums[i] = nums[j];
+        //     }else if(nums[i] !=nums[j]){
+        //         i++;
+        //         count=1;
+        //         nums[i] = nums[j];
+        //     }
+        // }
+
+        // return i+1;
+
+        int k = 2;
+
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
             }
         }
 
-        return i+1;
+        return k;        
     }
 }
+    
+
 
         
     
