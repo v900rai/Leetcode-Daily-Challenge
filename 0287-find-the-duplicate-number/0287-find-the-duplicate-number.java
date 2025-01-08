97eeca48b3b2abb8208cedf183da3d181b6b713a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-01-08 12:10:06 +0530

    Time: 4 ms (88.33%), Space: 58.1 MB (62.87%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,22 +1,24 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        // Using Floyd's Tortoise and Hare (Cycle Detection) Algorithm
         int slow = nums[0];
         int fast = nums[0];
 
-        // Phase 1: Detect the cycle
-        do {
+        while (true) {
             slow = nums[slow];
             fast = nums[nums[fast]];
-        } while (slow != fast);
 
-        // Phase 2: Find the entrance to the cycle (duplicate number)
-        slow = nums[0];
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
+            if (slow == fast) {
+                break;
+            }
         }
 
-        return slow;
+        int slow2 = nums[0];
+
+        while (slow != slow2) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+
+        return slow;        
     }
 }
