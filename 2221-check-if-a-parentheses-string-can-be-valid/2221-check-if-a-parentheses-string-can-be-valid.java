d06108cf487caa94c27dd51cd5deecd8ca4a260d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-01-12 11:51:33 +0530

    Time: 15 ms (83.4%), Space: 45.7 MB (50.21%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
@@ -1,24 +1,57 @@
-
 class Solution {
     public boolean canBeValid(String s, String locked) {
         int n = s.length();
-        if (n % 2 != 0) return false;
-
-        int open = 0, close = 0, unlocked1 = 0, unlocked2 = 0;
-        for (int i = 0; i < n; i++) {
-            // Left-to-right
-            if (locked.charAt(i) == '0') unlocked1++;
-            else if (s.charAt(i) == '(') open++;
-            else if (s.charAt(i) == ')') open--;
-            if (open + unlocked1 < 0) return false;
-
-            // Right-to-left
-            int j = n - i - 1;
-            if (locked.charAt(j) == '0') unlocked2++;
-            else if (s.charAt(j) == ')') close++;
-            else if (s.charAt(j) == '(') close--;
-            if (close + unlocked2 < 0) return false;
+        if (n % 2 != 0) {
+            // If the string length is odd, it cannot be valid
+            return false;
         }
+        
+        // Left-to-right pass
+        int openCount = 0; // Tracks potential '(' count
+        int balance = 0;   // Tracks current balance of parentheses
+        
+        for (int i = 0; i < n; i++) {
+            if (locked.charAt(i) == '1') {
+                // If the character is locked
+                if (s.charAt(i) == '(') {
+                    openCount++;
+                } else {
+                    openCount--;
+                }
+            } else {
+                // If the character is unlocked
+                openCount++; // Treat it as '(' for now
+            }
+            balance++;
+            
+            if (openCount < 0) {
+                // More ')' than '(' at this point, not valid
+                return false;
+            }
+        }
+        
+        // Right-to-left pass
+        int closeCount = 0; // Tracks potential ')' count
+        balance = 0;        // Reset balance for reverse pass
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (locked.charAt(i) == '1') {
+                if (s.charAt(i) == ')') {
+                    closeCount++;
+                } else {
+                    closeCount--;
+                }
+            } else {
+                closeCount++; // Treat it as ')' for now
+            }
+            balance++;
+            
+            if (closeCount < 0) {
+                // More '(' than ')' at this point, not valid
+                return false;
+            }
+        }
+        
         return true;
     }
 }
