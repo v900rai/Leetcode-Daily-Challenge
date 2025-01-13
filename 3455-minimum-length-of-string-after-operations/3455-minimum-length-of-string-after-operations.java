32c2f96e6cb788da0145fa69ec37dd6fe501b640
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-01-13 17:25:11 +0530

    Time: 9 ms (92.05%), Space: 46.2 MB (66.89%) - LeetHub

---
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumLength(String s) {
+
+        int freq[] = new int[26];
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            freq[s.charAt(i) - 'a']++;
+        }
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(freq[i]>0){
+                if(freq[i]%2==0){
+                    count+=2;
+                }else{
+                    count+=1;
+                }
+            }
+        }
+        return count;
+        
+    }
+}
