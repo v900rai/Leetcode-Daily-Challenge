e9688d57c75b7642721755e7e293cc6343e34682
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-01-24 00:47:44 +0530

    Time: 3 ms (39.29%), Space: 41.8 MB (77.06%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int total = 0;
+
+        // Add all characters of t to total
+        for (int i = 0; i < t.length(); i++) {
+            total += t.charAt(i);
+        }
+
+        // Subtract all characters of s from total
+        for (int i = 0; i < s.length(); i++) {
+            total -= s.charAt(i);
+        }
+
+        // The difference will be the extra character in t
+        return (char) total;
+    }
+}
