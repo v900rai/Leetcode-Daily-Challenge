ae2daa717c12b68c7870ad5f5b9ad94d4276c4e6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-01-25 20:26:29 +0530

    Time: 1 ms (99.6%), Space: 45.2 MB (41.21%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,12 +1,16 @@
 
 class Solution{
 public List<String>fizzBuzz(int n){
+// Arraylist 
 List<String>result=new ArrayList<>();
+//traversal full array
 for(int i=1;i<=n;i++){
 if(i%3==0&&i%5==0){
 result.add("FizzBuzz");
 }else if(i%3==0){
 result.add("Fizz");
+
+
 }else if(i%5==0){
 result.add("Buzz");
 }else{
