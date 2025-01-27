d7dd1013a5ae64c756de0521900570077a9a311a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-01-27 21:39:32 +0530

    Time: 0 ms (100%), Space: 42.8 MB (72.02%) - LeetHub

--- 1791-richest-customer-wealth/1791-richest-customer-wealth.java
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.java
@@ -1,14 +1,14 @@
 class Solution {
     public int maximumWealth(int[][] nums) {
-        int vishu = Integer.MIN_VALUE;
+        int vishu = 0;
         for(int i=0; i <nums.length;i++){
             int sum =0;
             for(int j =0 ;j<nums[i].length;j++){
                 sum+=nums[i][j];
             }
-            if(sum>vishu)
+            if(sum>=vishu)
             {
-                vishu =sum;
+                vishu=sum;
             }
         }
         return vishu;
