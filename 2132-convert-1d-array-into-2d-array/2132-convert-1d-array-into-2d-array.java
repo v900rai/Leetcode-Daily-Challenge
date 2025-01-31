5c34450b42ce64de4304017170b4fe4294fa5fd1
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-01-31 11:02:37 +0530

    Time: 4 ms (54.98%), Space: 55.6 MB (69.11%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
@@ -4,15 +4,15 @@ class Solution {
             return new int[0][0];
         }
         //initialize the 2d arrays
-        int [][] result=new int[m][n];
-        int index=0;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                result[i][j]=original[index];
-                index++;
-            }
+       int result[][]=new int [m][n];
+       int index=0;
+       for(int i=0; i<m; i++ ){
+        for(int j=0; j<n; j++){
+            result[i][j]=original[index];
+            index++;
         }
-        return result;
+       }
+       return result;
         
         
     }
