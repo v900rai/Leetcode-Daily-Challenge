e6808098f743a0353edf623e5ceca5a659f94259
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-01-31 10:49:43 +0530

    Time: 0 ms (100%), Space: 45.6 MB (37.44%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int m = nums.length;
+        int  n = nums[0].length;
+//dono ka length compared krna fir condition col,row both are not equal to length return nums
+        if (m * n != r * c) 
+        return nums;
+        
+        int[][] result = new int[r][c];
+        int row = 0, col = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[row][col] = nums[i][j];
+                col++;
+                if (col == c) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
