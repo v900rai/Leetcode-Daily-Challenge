3e92f7999917f974efe0b66f762efe73b954cbde
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-02-09 20:14:11 +0530

    Time: 1 ms (99.8%), Space: 53.4 MB (29.23%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,21 @@
 
-    class Solution {
+   class Solution {
     public int majorityElement(int[] nums) {
-        int vishalrai=0;
-        int count=0;
-        for(int i=0; i<nums.length; i++){
-            if(count==0){
-                vishalrai=nums[i];
-            }
-            if(vishalrai==nums[i]){
-                count++;
-            }
-            else{
-                count--;
+        int candidate = nums[0]; // पहले एलिमेंट को मेजॉरिटी मानते हैं
+        int count = 1; // काउंट शुरू में 1 रखते हैं
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == candidate) {
+                count++; // अगर वही संख्या मिली तो काउंट बढ़ाएँ
+            } else {
+                count--; // अन्यथा काउंट घटाएँ
+                if (count == 0) {
+                    candidate = nums[i]; // नया संभावित मेजॉरिटी एलिमेंट सेट करें
+                    count = 1;
+                }
             }
         }
-        return vishalrai;
-
+        
+        return candidate; // मेजॉरिटी एलिमेंट लौटाएँ
     }
-    }
+}
