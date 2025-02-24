e69ec5b865b31a8c27d2e8f1b9ca2809ef4a971c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-02-24 20:12:56 +0530

    Time: 2 ms (98.86%), Space: 45 MB (44.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,21 +1,39 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // nums: इनपुट के रूप में दिया गया integer array  
-        // target: वह संख्या जिसे हमें दो संख्याओं को जोड़कर प्राप्त करना है  
-        int n = nums.length; // Array की लंबाई प्राप्त करना  
+        // // nums: इनपुट के रूप में दिया गया integer array  
+        // // target: वह संख्या जिसे हमें दो संख्याओं को जोड़कर प्राप्त करना है  
+        // int n = nums.length; // Array की लंबाई प्राप्त करना  
 
-        // दो nested loops का उपयोग करके सभी संभावित जोड़ों की जाँच करना  
-        for (int i = 0; i < n - 1; i++) { // पहले संख्या के लिए loop  
-            for (int j = i + 1; j < n; j++) { // दूसरे संख्या के लिए loop  
+        // // दो nested loops का उपयोग करके सभी संभावित जोड़ों की जाँच करना  
+        // for (int i = 0; i < n - 1; i++) { // पहले संख्या के लिए loop  
+        //     for (int j = i + 1; j < n; j++) { // दूसरे संख्या के लिए loop  
                 
-                // यदि दो संख्याओं का योग target के बराबर है, तो उनकी indices return करें  
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j}; // दो indices वाले array को return करना  
-                }
-            }
-        }
+        //         // यदि दो संख्याओं का योग target के बराबर है, तो उनकी indices return करें  
+        //         if (nums[i] + nums[j] == target) {
+        //             return new int[]{i, j}; // दो indices वाले array को return करना  
+        //         }
+        //     }
+        // }
         
-        // यदि कोई सही जोड़ा नहीं मिला तो null return करें  
-        return null;
+        // // यदि कोई सही जोड़ा नहीं मिला तो null return करें  
+        // return null;
+
+
+        // approach 2
+         HashMap<Integer, Integer> Map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (Map.containsKey(diff)) {
+                return new int[] 
+                { Map.get(diff), i };
+            }
+
+            Map.put(num, i);
+        }
+
+        return new int[] {};
     }
 }
