95b1558b48457a57fff9576ed428dcf4706acac8
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-02-25 14:48:55 +0530

    Time: 7 ms (83.08%), Space: 58.3 MB (35.36%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.java
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.java
@@ -1,16 +1,23 @@
 class Solution {
-    public int numOfSubarrays(int[] arr) {
-    int odd = 0, even = 0, sum = 0;
-    for (int n : arr) {
-        if (n % 2 == 1) {
-            int temp = odd;
-            odd = even + 1;
-            even = temp;
+    public int numOfSubarrays(int[] nums) {
+        int odd = 0;  // अब तक मिले विषम योग वाले prefix की संख्या
+        int even = 1; // शुरू में 0 को भी एक सम (even) prefix मानेंगे
+        int sum = 0;  // अब तक का योग
+        int totalCount = 0; // विषम योग वाले subarrays की कुल संख्या
+        int MOD = 1000000007; // परिणाम को सीमित करने के लिए
+
+        for (int num : nums) {
+            sum += num; // वर्तमान तक का prefix sum
+            
+            // यदि sum odd है, तो odd की संख्या बढ़ाएँ
+            if (sum % 2 == 1) {
+                totalCount = (totalCount + even) % MOD;
+                odd++;  // यह sum अब odd prefixes में गिना जाएगा
+            } else {
+                totalCount = (totalCount + odd) % MOD;
+                even++; // यह sum अब even prefixes में गिना जाएगा
+            }
         }
-        else
-            ++even;
-        sum = (sum + odd) % 1000000007;
+        return totalCount;
     }
-    return sum;
-}
 }
