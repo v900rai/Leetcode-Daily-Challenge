a5fdd23b10cf926d6b58cd9c8a6a8e7f9728e71e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-02-25 17:48:16 +0530

    Time: 1 ms (95.98%), Space: 45.1 MB (96.01%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.java
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -1,14 +1,23 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        // नई सरणी बनाना जिसमें nums की लंबाई का दोगुना स्थान हो
-        int[] ans = new int[2 * nums.length]; 
+        // // नई सरणी बनाना जिसमें nums की लंबाई का दोगुना स्थान हो
+        // int[] ans = new int[2 * nums.length]; 
         
-        // nums को ans में दो बार कॉपी करना
-        for(int i = 0; i < nums.length; i++){
-            ans[i] = nums[i]; // पहली बार कॉपी करना
-            ans[i + nums.length] = nums[i]; // दूसरी बार कॉपी करना
+        // // nums को ans में दो बार कॉपी करना
+        // for(int i = 0; i < nums.length; i++){
+        //     ans[i] = nums[i]; // पहली बार कॉपी करना
+        //     ans[i + nums.length] = nums[i]; // दूसरी बार कॉपी करना
+        // }
+        
+        // return ans; // परिणामी सरणी लौटाना
+
+        int [] res=new int[2*nums.length];
+        for(int i=0; i<nums.length; i++){
+            //first time copy
+            res[i]=nums[i];
+            //seconds time copy
+            res[i +nums.length]=nums[i];
         }
-        
-        return ans; // परिणामी सरणी लौटाना
+        return res;
     }
 }
