409daf0049ee075c25e14f304df9aca6e1dbc346
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-06 15:58:33 +0530

    Time: 0 ms (100%), Space: 40.3 MB (79.69%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
@@ -1,20 +1,30 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-        int left = 1, right = n;
-        
-        while (left <= right) { // Condition changed from left < right to left <= right
-            int guess_no = left + (right - left) / 2;
-            int val = guess(guess_no);
-            
-            if (val == 0) {
+        int left=1;
+        int right=n;
+        while(left<=right){
+            int guess_no=left+(right-left)/2;
+            int val=guess(guess_no);
+            if(val==0){
                 return guess_no;
-            } else if (val == -1) {
-                right = guess_no - 1; // Move left
-            } else {
-                left = guess_no + 1;  // Move right
+            }
+            if(val==-1){
+                   right=guess_no-1;
+            }
+            else{
+                left=guess_no+1;
             }
         }
+          return 1;
         
-        return -1; // Should never reach here if input constraints are valid
     }
 }
