c7ca70db5298147387e40a2b503c909d9cfce995
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-13 00:32:54 +0530

    Time: 0 ms (100%), Space: 45.3 MB (25.27%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,16 +1,23 @@
 class Solution {
-    private static int bisect_left(int[] nums, int target) {
-        int lo = 0, hi = nums.length; 
-        while (lo < hi) {
-            int mid = lo + (hi - lo)/2; 
-            if (nums[mid] < target) lo = mid+1; 
-            else hi = mid; 
-        }
-        return lo; 
-    }
-    
     public int maximumCount(int[] nums) {
-        int neg = bisect_left(nums, 0), pos = nums.length - bisect_left(nums, 1); 
-        return Math.max(neg, pos); 
+        //first >-1 -> neg
+        // first >0 -> pos
+        int n = nums.length;
+        int end = n-1;
+        int negPos = binarySearch(nums, 0, end, -1); //log(n)
+        int posPos = binarySearch(nums, negPos, end, 0); //log(n - negPos)
+        int posCount = (n - posPos);
+        return Math.max(negPos,posCount);
+    }
+    public int binarySearch(int nums[], int start, int end, int target){
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]<=target){
+                start = mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return start;
     }
 }
