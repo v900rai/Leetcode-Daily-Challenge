b339f0c4bbccce9da6604a7ff357097f8a284717
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-13 16:02:39 +0530

    Time: 0 ms (100%), Space: 41.9 MB (86.31%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,16 +1,25 @@
 class Solution {
     public int findMin(int[] nums) {
+        // Initialize the left and right pointers
         int left = 0;
         int right = nums.length - 1;
+
+        // Perform binary search to find the minimum element
         while (left < right) {
+            // Find the middle index
             int mid = left + (right - left) / 2;
-            //
+
+            // If the middle element is less than or equal to the rightmost element,
+            // it means the minimum element lies in the left half (including mid)
             if (nums[mid] <= nums[right]) {
-                right = mid;
+                right = mid; // Move right pointer to mid
             } else {
+                // If nums[mid] is greater than nums[right], it means the minimum element
+                // lies in the right half, so we move left to mid + 1
                 left = mid + 1;
             }
         }
+        // At the end of the loop, left will point to the minimum element
         return nums[left];        
     }
 }
