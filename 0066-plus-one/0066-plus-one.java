dccb03ca9741171acf8c2106812ba119cc0721f8
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-15 00:01:49 +0530

    Time: 0 ms (100%), Space: 42.1 MB (17.13%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,22 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digit) {
-        int n=digit.length;
-        for(int i=digit.length-1; i>=0; i--){
-            
-            if(digit[i]!=9){
-                digit[i]++;
-                break;
-            }
-            else{
-                digit[i]=0;
-            }
-        }
-     if(digit[0]==0){
-        int [] res=new int [n+1];
-        res[0]=1;
-        return res;
-     }
-     return digit;
+      for(int i=digit.length-1; i>=0; i-- )
+      if(digit[i]==9){
+        digit[i]=0;
+      }
+      else{
+        digit[i]++;
+        return digit;
+      }
+      digit=new int[digit.length+1];
+      digit[0]=1;
+      return digit;
         
     }
 }
