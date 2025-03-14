e7b16e6ea75582734ec3b8f02da7b87cffb3413e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-14 23:30:53 +0530

    Time: 3 ms (42.59%), Space: 42.1 MB (29.74%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -3,9 +3,9 @@ class Solution {
         int total = 0;
 
         // Add all characters of t to total
-        for (int i = 0; i < t.length(); i++) {
-            total += t.charAt(i);
-        }
+       for(int i=0; i<t.length(); i++){
+        total=total+t.charAt(i);
+       }
 
         // Subtract all characters of s from total
         for (int i = 0; i < s.length(); i++) {
