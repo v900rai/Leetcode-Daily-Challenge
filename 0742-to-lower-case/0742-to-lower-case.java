f06a898ba263036d205a597d81b848bc89c48918
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-15 11:25:48 +0530

    Time: 1 ms (21.33%), Space: 41.6 MB (77.6%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,5 +1,15 @@
 class Solution {
-    public String toLowerCase(String s) {
-        return s.toLowerCase();
+  public String toLowerCase(String str) {
+    // inbuiled methods are use sometimes are not satiesfied interviewers' thats why
+    // i will use simple methods
+    // return s.toLowerCase();
+
+    // StringBuilder st=new StringBuilder();
+
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < str.length(); i++) {
+      sb.append(Character.toLowerCase(str.charAt(i)));
     }
+    return sb.toString();
+  }
 }
