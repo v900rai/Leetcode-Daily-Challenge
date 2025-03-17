a5dd552c25d36746258fc5224316ec79a2274eec
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-17 11:08:11 +0530

    Time: 3 ms (77.44%), Space: 44.5 MB (49.25%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -1,22 +1,13 @@
-import java.util.Arrays;
-
 class Solution {
     public boolean divideArray(int[] nums) {
-        // Sorting the array to group identical elements together
         Arrays.sort(nums);
         
-        // Traverse the array and check if all elements can form pairs
-        for (int i = 0; i < nums.length - 1; i++) {
-            // If two consecutive elements are the same, move to the next pair
-            if (nums[i] == nums[i + 1]) {
-                i++; // Increment i again to skip the next element in the pair
-                continue; // Continue checking the next pair
-            } 
-            else {
-                return false; // If a pair is not found, return false
+        for (int i = 1; i < nums.length; i += 2) {
+            if ((nums[i] ^ nums[i - 1]) != 0) {
+                return false;
             }
         }
         
-        return true; // If all elements can form pairs, return true
+        return true;
     }
 }
