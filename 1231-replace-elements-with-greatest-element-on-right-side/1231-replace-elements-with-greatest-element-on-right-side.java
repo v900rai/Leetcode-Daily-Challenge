a751448673311222d52ddeffe5f9fc707f1aff2e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-20 11:43:08 +0530

    Time: 2 ms (97.72%), Space: 45.7 MB (99.49%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.java
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.java
@@ -1,10 +1,14 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-        int max = -1; // अंतिम तत्व के लिए -1 होना चाहिए
-        for (int i = arr.length - 1; i >= 0; i--) {
-            int temp = arr[i]; // वर्तमान तत्व को सहेजें
-            arr[i] = max; // वर्तमान स्थान पर अधिकतम दाएँ तत्व को रखें
-            max = Math.max(max, temp); // अधिकतम मान को अपडेट करें
+        int max=-1;
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i]>max){
+                int temp=max;
+                max=arr[i];
+                arr[i]=temp;
+            }
+            else
+                arr[i]=max;
         }
         return arr;
     }
