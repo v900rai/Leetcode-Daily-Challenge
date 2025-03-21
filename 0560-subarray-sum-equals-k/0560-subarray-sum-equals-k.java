76f3cfa949d733d66dc006037639953fa38f3b9b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-21 13:17:29 +0530

    Time: 1076 ms (27.67%), Space: 46.5 MB (53.34%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,22 +1,17 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int n = nums.length;
-        int count = 0;
-
-        // बाहरी लूप सभी संभावित उपसरणी (subarray) की शुरुआत को दर्शाता है
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-
-            // आंतरिक लूप i से शुरू होने वाली सभी उपसरणियों का योग निकालता है
-            for (int j = i; j < n; j++) {
-                sum += nums[j]; // वर्तमान उपसरणी का योग
-                
-                // यदि योग k के बराबर है, तो उपसरणी की गणना करें
-                if (sum == k) {
-                    count++;
-                }
-            }
+      int count=0;
+      int n=nums.length;
+      for(int i=0; i<n; i++){
+        int sum=0;
+        for(int j=i; j<n; j++){
+          sum=sum+nums[j];
+          if(sum==k){
+            count++;
+          }
         }
-        return count; // कुल उपसरणी की संख्या लौटाएँ जिनका योग k है
+      }
+      return count;
+        
     }
 }
