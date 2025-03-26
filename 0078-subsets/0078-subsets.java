8d4b0ecbdce41cd6e566fd6400f6b5c968a15cbb
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-26 19:39:31 +0530

    Time: 1 ms (53.1%), Space: 42.8 MB (46.46%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,21 +1,22 @@
-
 class Solution {
-    public List<List<Integer>> subsets(int[] arr) {
-        List<Integer> curr = new ArrayList<Integer>();
-		List<List<Integer>> ans = new ArrayList<>();
-		rec(arr, 0, curr, ans);
-        return ans;
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+
+        createSubset(nums, 0, res, subset);
+        return res;        
     }
-    
-    public void rec(int[] arr, int i, List<Integer> curr, List<List<Integer>> ans) {
-		if (i == arr.length) {
-			ans.add(new ArrayList<Integer>(curr));
-			return;
-		}
-		curr.add(arr[i]);
-		rec(arr, i + 1, curr, ans);
-		curr.remove(curr.size() - 1);
-		rec(arr, i + 1, curr, ans);
-        
-	}
+
+    private void createSubset(int[] nums, int index, List<List<Integer>> res, List<Integer> subset) {
+        if (index == nums.length) {
+            res.add(new ArrayList<>(subset));
+            return;
+        }
+
+        subset.add(nums[index]);
+        createSubset(nums, index + 1, res, subset);
+
+        subset.remove(subset.size() - 1);
+        createSubset(nums, index + 1, res, subset);
+    }    
 }
