35dd65e1cc4b0c2fac71b00da53590375b32d719
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-29 01:02:03 +0530

    Time: 0 ms (100%), Space: 56.7 MB (14.75%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -1,19 +1,20 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int left = 0, right = arr.length - 1;
-
-        while (left < right) {  // '=' लगाने की जरूरत नहीं है
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] > arr[mid + 1]) {
-                // Peak left side में हो सकती है, इसलिए right को mid पर लाते हैं
-                right = mid;
-            } else {
-                // Peak right side में हो सकती है
-                left = mid + 1;
-            }
+      int left=0;
+      int right=arr.length-1;
+      while(left<=right){
+        int mid=left+(right-left)/2;
+        if(arr[mid-1]<arr[mid]&& arr[mid]>arr[mid+1])
+        return mid;
+        else if(arr[mid-1]<arr[mid])
+        left=mid+1;
+        else{
+          right=mid;
         }
-
-        return left;  // Left pointer ही peak index पर आ जाएगा
+      }
+      // this ie adage  cases 
+      return -1;
+      //this not execute 
+        
     }
 }
