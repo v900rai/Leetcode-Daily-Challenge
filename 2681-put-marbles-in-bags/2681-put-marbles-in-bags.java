c9b6e34c5adf2fabb8c81c0e9f3ea4ca65dbfaf6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-31 12:17:54 +0530

    Time: 35 ms (47.76%), Space: 63.4 MB (32.09%) - LeetHub

---
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long putMarbles(int[] weights, int k) {
+        int n = weights.length;
+        if (k == n) return 0;
+        int[] W = new int[n-1];
+        for (int i = 1; i < n; i++) W[i-1] = weights[i] + weights[i-1];
+        Arrays.sort(W);
+
+        return max(W, k-1) - min(W, k-1);
+    }
+    
+    private long max(int[] W, int k) {
+        int n = W.length;
+        long res = 0;
+        for (int i = n-1; i >= n-k; i--) res += W[i];
+        return res;
+    }
+
+    private long min(int[] W, int k) {
+        long res = 0;
+        for (int i = 0; i < k; i++) res += W[i];
+        return res;
+    }
+}
