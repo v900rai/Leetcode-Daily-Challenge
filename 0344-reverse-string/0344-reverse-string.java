25ae36d5e1e914726692997f79e6279939981a90
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-04-09 19:15:31 +0530

    Time: 0 ms (100%), Space: 45.6 MB (90.08%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,34 +1,14 @@
 class Solution {
-
-  public void reverseString(char[] s) {
-    int left=0;
-    int right=s.length-1;
-    while(left<right){
+    public void reverseString(char[] s) {
+      int left=0;
+      int right= s.length-1;
+      while(left<right){
         char temp=s[left];
         s[left]=s[right];
         s[right]=temp;
         left++;
         right--;
+      }
+        
     }
-  }
 }
-
-
-//      public class Solution {
-//     public String reverseString(String s) {
-
-    // jb String parameterized m string s to us ko char k cob=nvert krna padega
-//         char[] word = s.toCharArray();
-//         int i = 0;
-//         int j = s.length() - 1;
-//         while (i < j) {
-//             char temp = word[i];
-//             word[i] = word[j];
-//             word[j] = temp;
-//             i++;
-//             j--;
-//         }
-//         return new String(word);
-//     }
-// }   
-    
