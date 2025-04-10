316ff513d28e58075354001896182b36f33395a8
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-04-10 17:42:00 +0530

    Time: 2 ms (76.06%), Space: 42.6 MB (67.61%) - LeetHub

---
+++ 3243-count-the-number-of-powerful-integers/3243-count-the-number-of-powerful-integers.java
@@ -0,0 +1,34 @@
+class Solution {
+    private long solve(String str, String suffix, int limit) {
+        if (str.length() < suffix.length()) {
+            return 0;
+        }
+
+        long count = 0;
+        String trailing = str.substring(str.length() - suffix.length());
+        int prefixLength = str.length() - suffix.length();
+
+        for (int i = 0; i < prefixLength; i++) {
+            int digit = str.charAt(i) - '0';
+
+            if (digit <= limit) {
+                count += digit * Math.pow(limit + 1, prefixLength - i - 1);
+            } else {
+                count += Math.pow(limit + 1, prefixLength - i);
+                return count;
+            }
+        }
+
+        if (trailing.compareTo(suffix) >= 0) {
+            count++;
+        }
+
+        return count;
+    }
+
+    public long numberOfPowerfulInt(long start, long finish, int limit, String s) {
+        String startStr = String.valueOf(start - 1);
+        String finishStr = String.valueOf(finish);
+        return solve(finishStr, s, limit) - solve(startStr, s, limit);
+    }
+}
