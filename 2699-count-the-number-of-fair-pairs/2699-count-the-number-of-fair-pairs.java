1f9baa99863a433bcfbb1208af6da18e9e7308dc
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-04-19 16:48:15 +0530

    Time: 23 ms (100%), Space: 57 MB (91.57%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
@@ -6,10 +6,12 @@ class Solution {
 
     private long countPairs(int[] nums, int target) {
         long count = 0;
-        int left = 0, right = nums.length - 1;
+        int left = 0;
+        int  right = nums.length - 1;
         
         while (left < right) {
-            if (nums[left] + nums[right] > target) right--;
+            if (nums[left] + nums[right] > target) 
+            right--;
             else {
                 count += right - left;
                 left++;
