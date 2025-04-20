acb920328c0540fe0ad6a240dd4c2ce754533afa
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-04-20 22:17:01 +0530

    Time: 30 ms (48.76%), Space: 45.3 MB (65.57%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,24 +1,47 @@
+// class Solution {
+//     public int firstUniqChar(String s) {
+//         // Step 1: Create an array to store character frequencies
+//         int[] count = new int[26]; // Assuming only lowercase English letters
+        
+//         // Step 2: Count the frequency of each character in the string
+//         for(char ch: s.toCharArray()){
+//             count[ch-'a']++;
+            
+//         }
+        
+//         // Step 3: Find the first character with a frequency of 1
+//         int n=s.length();
+//         for (int i = 0; i < n; i++) {
+//             char ch=s.charAt(i);
+//             if(count[ch-'a']==1){
+//                 return i;
+//             }
+//         }
+        
+//         // Step 4: If no unique character is found, return -1
+//         return -1;
+//     }
+// }
+
+
+
+
 class Solution {
     public int firstUniqChar(String s) {
-        // Step 1: Create an array to store character frequencies
-        int[] count = new int[26]; // Assuming only lowercase English letters
-        
-        // Step 2: Count the frequency of each character in the string
-        for(char ch: s.toCharArray()){
-            count[ch-'a']++;
-            
+        HashMap<Character, Integer> mp = new HashMap<>();
+
+        for (char a : s.toCharArray()) {
+            mp.put(a, mp.getOrDefault(a, 0) + 1);
         }
-        
-        // Step 3: Find the first character with a frequency of 1
-        int n=s.length();
-        for (int i = 0; i < n; i++) {
-            char ch=s.charAt(i);
-            if(count[ch-'a']==1){
+
+        for (int i = 0; i < s.length(); i++) {
+            if (mp.get(s.charAt(i)) == 1) {
                 return i;
             }
         }
-        
-        // Step 4: If no unique character is found, return -1
+
         return -1;
     }
 }
+
+
