e04e6cfccf17c818dedbb6d5afedded5017435ae
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-04-26 10:58:48 +0530

    Time: 836 ms (14.46%), Space: 52.5 MB (24.55%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,21 +1,40 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> res=new HashSet<>();
-        if(nums.length==0)
-        return new ArrayList<>(res);
+        // Using a Set to store the result to automatically handle duplicate triplets
+        Set<List<Integer>> res = new HashSet<>();
+
+        // Edge case: if array is empty, return empty list
+        if (nums.length == 0)
+            return new ArrayList<>();
+
+        // Step 1: Sort the array to use two-pointer technique
         Arrays.sort(nums);
-        for(int i=0; i<nums.length-2; i++){
-            int j=i+1;
-            int k=nums.length-1;
-            while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum==0)
-                 res.add(Arrays.asList(nums[i],nums[j++], nums[k--]));
-                else if(sum>0) k--;
-                else if(sum<0) j++;
+
+        // Step 2: Traverse each element, treating it as the first element of triplet
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1; // Left pointer
+            int k = nums.length - 1; // Right pointer
+
+            // Step 3: Move the two pointers to find valid triplets
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k]; // Calculate current triplet sum
+
+                if (sum == 0) {
+                    // Valid triplet found, add to result set
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++; // Move both pointers to check for more possibilities
+                    k--;
+                } else if (sum > 0) {
+                    k--; // Sum is too big, move right pointer left to reduce it
+                } else {
+                    j++; // Sum is too small, move left pointer right to increase it
+                }
             }
         }
-        return new ArrayList<> (res);
-        
+
+        // Convert the Set to List and return
+        return new ArrayList<>(res);
     }
 }
