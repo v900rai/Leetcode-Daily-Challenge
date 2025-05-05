384815c05130038312201ae3f02603e367e0690e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-05-05 14:39:41 +0530

    Time: 1 ms (83.82%), Space: 41.2 MB (24.64%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int reverse(int num) {
-        long reversedNumber = 0;  
-        
-        while (num != 0) {
-            int digit = num % 10;  
-            reversedNumber = reversedNumber * 10 + digit;  
-            num /= 10;  
+  public int reverse(int num) {
+    long reversedNumber = 0;
 
-            // Overflow check (Integer limit से बाहर जाने पर return 0)
-            if (reversedNumber > Integer.MAX_VALUE || reversedNumber < Integer.MIN_VALUE) {
-                return 0;
-            }
-        }
-        
-        return (int) reversedNumber;  
+    while (num != 0) {
+      int digit = num % 10;
+      reversedNumber = reversedNumber * 10 + digit;
+      num /= 10;
+
+      // Overflow check (Integer limit से बाहर जाने पर return 0)
+      if (reversedNumber > Integer.MAX_VALUE || reversedNumber < Integer.MIN_VALUE) {
+        return 0;
+      }
     }
+
+    return (int) reversedNumber;
+  }
 }
