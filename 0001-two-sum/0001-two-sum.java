f51fbf66b47a8de66089161bd7512e94c02c35da
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-05-24 16:50:13 +0530

    Time: 39 ms (37.74%), Space: 45 MB (63.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-
-        // हर संख्या को दूसरे के साथ जोड़कर चेक करना
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};  // सही इंडेक्स return करो
-                }
-            }
+      int n=nums.length;
+      for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+          if(nums[i]+nums[j]==target){
+            return new int[] {i,j};
+          }
         }
+      }
+        return new int[] { };
       
-        return new int[]{};  // अगर कोई pair नहीं मिला
+        
     }
 }
