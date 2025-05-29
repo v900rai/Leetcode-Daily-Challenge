03547ff4c3823fb40697988dfc0d404c7969450c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-05-29 14:34:22 +0530

    Time: 0 ms (100%), Space: 44.9 MB (98.49%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,20 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int ans = 0;
-
-        // XOR all numbers from 0 to n
-        for (int i = 0; i <= n; i++) {
-            ans = ans ^ i;
-        }
-
-        // XOR all elements in the nums array
-        for (int i = 0; i < n; i++) {
-            ans = ans ^ nums[i];
-        }
-
-        return ans;
+       int res=nums.length;
+       for(int i=0; i<nums.length; i++){
+        res+=i-nums[i];
+       }
+       return res;
     }
+  
+
 }
 
     
