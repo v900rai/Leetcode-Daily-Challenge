5c47fddccdc9924b8aa688a5a723c1e72c1a4dd8
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-06-03 19:36:20 +0530

    Time: 2 ms (100%), Space: 44.8 MB (69.08%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,8 +1,11 @@
 class Solution {
-     public int romanToInt(String s) {
-         int ans = 0;
-         int num = 0;
-        for (int i = s.length()-1; i >= 0; i--) {
+    public int romanToInt(String s) {
+        int ans = 0;  // Final result
+        int num = 0;  // Holds the value of the current Roman character
+
+        // Traverse the string from right to left
+        for (int i = s.length() - 1; i >= 0; i--) {
+            // Map the current Roman character to its corresponding integer value
             switch(s.charAt(i)) {
                 case 'I': num = 1; break;
                 case 'V': num = 5; break;
@@ -12,10 +15,16 @@ class Solution {
                 case 'D': num = 500; break;
                 case 'M': num = 1000; break;
             }
-            if (4 * num < ans) ans -= num;
-            else ans += num;
+
+            // If current value is less than one-fourth of ans, subtract it
+            // This handles cases like IV (4), IX (9), etc.
+            if (4 * num < ans) {
+                ans -= num;
+            } else {
+                ans += num;
+            }
         }
-        return ans;
+
+        return ans;  // Return the final integer value
     }
-   
 }
