b1188f37741911d351ba8c4dcebf212b4aba1416
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-06-23 12:38:40 +0530

    Time: 2 ms (93.17%), Space: 47.1 MB (78.88%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,22 +1,34 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int left=0;
-        int right=numbers.length-1;
-        while(left<right){
-            int sum=numbers[left]+numbers[right];
-            if(sum==target){
-                return new int []{left+1, right+1};
-            }
-            else if(sum>target){
-                right--;
-                
+        // Initialize two pointers: 
+        // 'left' starts at the beginning of the array (index 0)
+        // 'right' starts at the end of the array (index numbers.length - 1)
+        int left = 0;
+        int right = numbers.length - 1;
 
-            }
-            else{
+        // Loop until the two pointers meet
+        while (left < right) {
+            // Calculate the sum of the elements at the current left and right pointers
+            int sum = numbers[left] + numbers[right];
+
+            // If the sum matches the target, return the 1-based indices of the two numbers
+            if (sum == target) {
+                // The problem expects 1-based indexing, so we add 1 to each index
+                return new int[]{left + 1, right + 1};
+            } 
+            // If the sum is greater than the target, move the right pointer to the left
+            // This decreases the sum because the array is sorted in ascending order
+            else if (sum > target) {
+                right--;
+            } 
+            // If the sum is less than the target, move the left pointer to the right
+            // This increases the sum because the array is sorted in ascending order
+            else {
                 left++;
             }
         }
-        return new int []{-1,-1};//if no solution is found
-        
+
+        // If no solution is found (though the problem guarantees one), return [-1, -1]
+        return new int[]{-1, -1};
     }
 }
