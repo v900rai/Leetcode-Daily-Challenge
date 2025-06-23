57877858610e97dae4f1106ea6e10b99ae2dc89a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-06-23 12:51:52 +0530

    Time: 0 ms (100%), Space: 57.6 MB (35.65%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,30 +1,47 @@
-class Solution {
-  public void rotate(int[] nums, int k) {
-    if (nums == null || nums.length == 0 || k <= 0) {
-      return;
-    }
+// class Solution {
+//   public void rotate(int[] nums, int k) {
+//     if (nums == null || nums.length == 0 || k <= 0) {
+//       return;
+//     }
 
-    int n = nums.length;
-    k = k % n; // In case k is greater than the length of the array
+//     int n = nums.length;
+//     k = k % n; // In case k is greater than the length of the array
 
-    // Reverse the entire array
-    reverse(nums, 0, n - 1);//while arrays
-    // Reverse the first k elements
+//     // Reverse the entire array
+//     reverse(nums, 0, n - 1);//while arrays
+//     // Reverse the first k elements
+//     reverse(nums, 0, k - 1);
+
+//     // Reverse the remaining n - k elements
+//     reverse(nums, k, n - 1);
+
+//   }
+
+//   private static void reverse(int[] nums, int start, int end) {
+//     while (start < end) {
+//       int temp = nums[start];
+//       nums[start] = nums[end];
+//       nums[end] = temp;
+//       start++;
+//       end--;
+//     }
+
+//   }
+// }
+class Solution{
+
+public void rotate(int[] nums, int k) {
+    k %= nums.length;
+    reverse(nums, 0, nums.length - 1);
     reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+}
 
-    // Reverse the remaining n - k elements
-    reverse(nums, k, n - 1);
-
-  }
-
-  private static void reverse(int[] nums, int start, int end) {
+private void reverse(int[] nums, int start, int end) {
     while (start < end) {
-      int temp = nums[start];
-      nums[start] = nums[end];
-      nums[end] = temp;
-      start++;
-      end--;
+        int temp = nums[start];
+        nums[start++] = nums[end];
+        nums[end--] = temp;
     }
-
-  }
+}
 }
