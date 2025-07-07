a072dc1fdf79d542f305a2b382dcb5418f4ff61c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-07 14:26:04 +0530

    Time: 0 ms (100%), Space: 42.3 MB (7.99%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,31 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        // Initialize two pointers, fast and slow, both starting at the head of the list
-        ListNode fast = head;
-        ListNode slow = head;
-
-        // Move the fast pointer n nodes ahead to create a gap of n nodes between fast and slow
-        for (int i = 0; i < n; i++) {
-            fast = fast.next;
+        ListNode dummy = new ListNode(0); // डमी नोड (Dummy node)
+        dummy.next = head;
+        
+        ListNode first = dummy; // पहला पॉइंटर (First pointer)
+        ListNode second = dummy; // दूसरा पॉइंटर (Second pointer)
+        
+        // पहले पॉइंटर को n+1 स्थान आगे ले जाएं (Move first pointer n+1 steps ahead)
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
         }
-
-        // If fast becomes null after moving n nodes, it means the node to be removed is the head
-        // So, return head.next (skip the head node)
-        if (fast == null) {
-            return head.next;
+        
+        // दोनों पॉइंटरों को एक साथ आगे बढ़ाएं (Move both pointers together)
+        while (first != null) {
+            first = first.next;
+            second = second.next;
         }
-
-        // Move both pointers until fast reaches the last node (fast.next == null)
-        // At this point, slow will be pointing to the node just before the node to be removed
-        while (fast.next != null) {
-            fast = fast.next;
-            slow = slow.next;
-        }
-
-        // Remove the nth node from the end by skipping it (slow.next = slow.next.next)
-        slow.next = slow.next.next;
-
-        // Return the head of the modified list
-        return head;
+        
+        // nth नोड को हटाएं (Remove nth node)
+        second.next = second.next.next;
+        
+        return dummy.next; // नया हेड (New head)
     }
 }
