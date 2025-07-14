53c5a57c0ef4129b9a2cc8ae0188c3ac3b82ee7a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-14 22:39:20 +0530

    Time: 0 ms (100%), Space: 42.3 MB (59.05%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -8,11 +8,19 @@ class Solution {
     int right = n - 1;
 
     // Step 1: Transpose the matrix (swap rows and columns)
-    for (int i = 0; i < n; i++) {
-      for (int j = i; j < n; j++) {
-        int temp = arr[i][j];
-        arr[i][j] = arr[j][i];
-        arr[j][i] = temp;
+    // for (int i = 0; i < n; i++) {
+    //   for (int j = i; j < n; j++) {
+    //     int temp = arr[i][j];
+    //     arr[i][j] = arr[j][i];
+    //     arr[j][i] = temp;
+    //   }
+    // }
+
+    for(int i=0; i<n; i++){
+      for(int j=i; j<n; j++){
+        int temp=arr[i][j];
+        arr[i][j]=arr[j][i];
+        arr[j][i]=temp;
       }
     }
 
@@ -29,4 +37,5 @@ class Solution {
       }
     }
   }
+  
 }
