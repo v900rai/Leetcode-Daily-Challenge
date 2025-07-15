7492e53ad76b8a5afc1306d055c88c39c4500698
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-15 11:32:03 +0530

    Time: 1 ms (99.12%), Space: 42.3 MB (26.49%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) {
+            return false;
+        }
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        for (char ch : word.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                ch = Character.toLowerCase(ch);
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            } else if (!Character.isDigit(ch)) {
+                return false;
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+}
