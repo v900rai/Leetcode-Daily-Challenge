2efa0392079936d99f255793dd5243205345936f
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-22 12:05:15 +0530

    Time: 0 ms (100%), Space: 41.5 MB (62.78%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,16 +1,33 @@
-class Solution {
+// class Solution {
   
-    public String longestCommonPrefix(String[] v) {
-        StringBuilder ans = new StringBuilder();
-        Arrays.sort(v);
-        String first = v[0];
-        String last = v[v.length-1];
-        for (int i=0; i<Math.min(first.length(), last.length()); i++) {
-            if (first.charAt(i) != last.charAt(i)) {
-                return ans.toString();
-            }
-            ans.append(first.charAt(i));
+//     public String longestCommonPrefix(String[] v) {
+//         StringBuilder ans = new StringBuilder();
+//         Arrays.sort(v);
+//         String first = v[0];
+//         String last = v[v.length-1];
+//         for (int i=0; i<Math.min(first.length(), last.length()); i++) {
+//             if (first.charAt(i) != last.charAt(i)) {
+//                 return ans.toString();
+//             }
+//             ans.append(first.charAt(i));
+//         }
+//         return ans.toString();
+//     }
+// }
+class Solution {
+  public String longestCommonPrefix(String[] strs) {
+    if (strs == null || strs.length == 0) 
+    return "";
+    
+    String prefix = strs[0];
+    
+    for (int i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) != 0) {
+            prefix = prefix.substring(0, prefix.length() - 1);
+            if (prefix.isEmpty()) return "";
         }
-        return ans.toString();
     }
+    
+    return prefix;
+}
 }
