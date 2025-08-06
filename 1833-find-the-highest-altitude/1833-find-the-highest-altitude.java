b3dce58ecf4c61e8c9c9ca51390c542cc551fffb
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-08-06 12:17:55 +0530

    Time: 0 ms (100%), Space: 41.3 MB (74.55%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.java
@@ -1,27 +1,13 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-    //   int sum=0;
-    //   int max=0;
-    //   for(int i=0; i<gain.length; i++){
-    //     sum=sum+gain[i];
-    //     max=Math.max(max,sum);
+      int sum=0;
+      int max=0;
+      for(int i=0; i<gain.length; i++){
+        sum=sum+gain[i];
+        max=Math.max(max,sum);
 
-    //   }
-    //   return max;
-
-
-  
-        int currentAltitude = 0;  // Tracks the current altitude
-        int highestAltitude = 0;  // Tracks the highest altitude reached
-
-        for (int change : gain) {
-            currentAltitude += change;  // Update the current altitude
-            highestAltitude = Math.max(highestAltitude, currentAltitude);  // Update the highest altitude if needed
-        }
-
-        return highestAltitude;  // Return the maximum altitude reached
-    
-
+      }
+      return max;
 
     }
 }
