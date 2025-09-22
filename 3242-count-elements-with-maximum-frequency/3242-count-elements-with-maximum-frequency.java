41b607e19ca3c41821949f1f58c33493916276c4
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-22 09:48:40 +0530

    Time: 1 ms (99.44%), Space: 41.7 MB (99.38%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
@@ -1,27 +1,21 @@
-
-public class Solution {
+class Solution {
     public int maxFrequencyElements(int[] nums) {
-        // Step 1: Frequency Calculation
-        Map<Integer, Integer> um = new HashMap<>();
-        for (int x : nums) {
-            um.put(x, um.getOrDefault(x, 0) + 1);
+        int maxval=0;
+        for(int i=0; i<nums.length; i++){
+            if(maxval <= nums[i]) maxval=nums[i];
         }
-
-        // Step 2: Find Maximum Frequency
-        int maxi = 0;
-        for (int value : um.values()) {
-            maxi = Math.max(maxi, value);
+        int arr[]=new int[maxval+1];
+        for(int i=0; i<nums.length; i++){
+            int ele=nums[i];
+            arr[ele]++;
         }
-
-        // Step 3: Calculate Total Frequency with Maximum Frequency
-        int ans = 0;
-        for (int value : um.values()) {
-            if (value == maxi) {
-                ans += value;
-            }
+        int max=0, ans=0;
+        for(int i=0; i<arr.length; i++){
+            if(max <= arr[i]) max=arr[i]; 
         }
-
-        // Step 4: Return Result
+        for(int i=0; i<arr.length; i++){
+            if(max == arr[i]) ans+=arr[i];
+        }
         return ans;
     }
 }
