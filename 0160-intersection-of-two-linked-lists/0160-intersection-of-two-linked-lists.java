2bf34fde9512a746edb4ce056f6d604f2a379c7d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-25 18:57:18 +0530

    Time: 585 ms (5.06%), Space: 49.2 MB (9.48%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,21 +10,18 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if (headA == null || headB == null) 
-        return null;
-        
-        ListNode a = headA;
-        ListNode b = headB;
-        
-        // जब तक वे इंटरसेक्ट नहीं करते (Until they intersect)
-        while (a != b) {
-            // अगर एक लिस्ट खत्म हो जाए, दूसरी के हेड पर शुरू करें
-            // (If one list ends, start from head of other)
-            a = a == null ? headB : a.next;
-            b = b == null ? headA : b.next;
+   public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        while(headA!=null)
+        {
+            ListNode temp = headB;
+            while(temp!=null)
+            {
+                if(temp==headA)
+                    return headA;
+                temp = temp.next;
+            }
+            headA = headA.next;
         }
-        
-        return a; // इंटरसेक्शन पॉइंट या null (Intersection point or null)
+        return null;
     }
 }
