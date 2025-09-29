be8e2bc849cf276fd62e1a0631d0fe8676596d91
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-29 13:03:33 +0530

    Time: 0 ms (100%), Space: 41.7 MB (93.5%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,28 +1,32 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        int low = 0;
-        int high = nums.length - 1;
-        int mid = 0;
-        
-        while (mid <= high) {
-          
-            if (nums[mid] == 0) {
-                swap(nums, mid, low);
-                low++;
-                mid++;
-            } else if (nums[mid] == 1) {
-                mid++;
-            } else {
-                swap(nums, mid, high);
-                high--;
-            }
-        }
-    }
+  public void sortColors(int[] nums) {
+    int low = 0;
+    int high = nums.length - 1;
+    int mid = 0;
 
-  
-    public static void swap(int nums[], int left , int right){
-      int temp =nums[left];
-      nums[left] = nums[right];
-      nums[right] =temp ;
+    while (mid <= high) {
+
+      if (nums[mid] == 0) {
+        swap(nums, mid, low);
+        low++;
+        mid++;
+      } else if (nums[mid] == 1) {
+        mid++;
+      } else {
+        swap(nums, mid, high);
+        high--;
+      }
     }
+  }
+
+  // public static void swap(int nums[], int left, int right) {
+  //   int temp = nums[left];
+  //   nums[left] = nums[right];
+  //   nums[right] = temp;
+  // }
+  public static  void swap(int nums[], int left, int right){
+    int temp=nums[left];
+    nums[left]=nums[right];
+    nums[right]=temp;
+  }
 }
