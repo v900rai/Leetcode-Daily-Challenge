387b0b961df89ab784bb5bca22d8d9fac60dc33e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-08 22:05:56 +0530

    Time: 0 ms (100%), Space: 41.8 MB (93.93%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,7 +3,7 @@ class Solution {
       int count =0;
       for(int i=0; i<nums.length; i++){
         if(nums[i]!=val){
-          nums[count]=nums[i];
+          nums[count] = nums[i];
           count++;
         }
       }
