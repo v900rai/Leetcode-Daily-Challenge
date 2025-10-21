2fc005f16a826a2b22cc56b78745bde131e7ab38
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-21 19:27:58 +0530

    Time: 0 ms (100%), Space: 41.4 MB (55.09%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+  public ListNode swapPairs(ListNode head) {
+
+    ListNode prevNode = new ListNode(0); //Node situated before the two to-be-swapped nodes
+    prevNode.next = head; //Starting before head
+    ListNode newHead = prevNode; //Storing a reference for returning
+
+    while (prevNode.next != null && prevNode.next.next != null) {
+
+      //Declare                                           // ...[prev]-->[node1]-->[node2]-->[next]...
+      ListNode node1 = prevNode.next;
+      ListNode node2 = node1.next;
+      ListNode nextNode = node2.next;
+
+      //Swap
+      prevNode.next = node2; // ...[prev]-->[node2]<--[node1]   [next]...
+      node2.next = node1; // ...[prev]-->[node2]<-->[node1]  [next]...
+      node1.next = nextNode; // ...[prev]-->[node2]-->[node1]-->[next]...
+
+      //Move 
+      prevNode = node1; // ...[...]-->[node2]-->[node1/prev]-->[next]...
+    }
+
+    return newHead.next;
+  }
+}
