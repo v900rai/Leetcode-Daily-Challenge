f8da773598e865a0677564f390a2f2bd7abae00a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-25 19:32:53 +0530

    Time: 1 ms (99.64%), Space: 45.2 MB (60.2%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,23 +1,22 @@
 
-class Solution{
-public List<String>fizzBuzz(int n){
-// Arraylist 
-List<String>result=new ArrayList<>();
-//traversal full array
-for(int i=1;i<=n;i++){
-if(i%3==0&&i%5==0){
-result.add("FizzBuzz");
-}else if(i%3==0){
-result.add("Fizz");
+class Solution {
+  public List<String> fizzBuzz(int n) {
+    // Arraylist 
+    List<String> result = new ArrayList<>();
+    //traversal full array
+    for (int i = 1; i <= n; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        result.add("FizzBuzz");
+      } else if (i % 3 == 0) {
+        result.add("Fizz");
 
+      } else if (i % 5 == 0) {
+        result.add("Buzz");
+      } else {
 
-}else if(i%5==0){
-result.add("Buzz");
-}else{
-    
-result.add(Integer.toString(i));
-}
-}
-return result;
-}
+        result.add(Integer.toString(i));
+      }
+    }
+    return result;
+  }
 }
