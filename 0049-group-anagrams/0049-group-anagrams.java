e55f40839b467018de6b62cb87556bbb1e42c0fb
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-11-02 19:48:48 +0530

    Time: 7 ms (62.23%), Space: 49.5 MB (5.7%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         // एक HashMap जिसका key होगा sorted string और value होगी words की list जो anagram हैं
