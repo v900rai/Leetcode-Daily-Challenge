4f4e8857a5d78a11230e2ba51b3404e8b129f6b4
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-11-13 18:42:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,52 +1,17 @@
-// class Solution {
-//     public int firstUniqChar(String s) {
-//         // Step 1: Create an array to store character frequencies
-//         int[] count = new int[26]; // Assuming only lowercase English letters
-        
-//         // Step 2: Count the frequency of each character in the string
-//         for(char ch: s.toCharArray()){
-//             count[ch-'a']++;
-            
-//         }
-        
-//         // Step 3: Find the first character with a frequency of 1
-//         int n=s.length();
-//         for (int i = 0; i < n; i++) {
-//             char ch=s.charAt(i);
-//             if(count[ch-'a']==1){
-//                 return i;
-//             }
-//         }
-        
-//         // Step 4: If no unique character is found, return -1
-//         return -1;
-//     }
-// }
-
-
-
 class Solution {
     public int firstUniqChar(String s) {
-        // Step 1: Create a HashMap to store frequency of each character
-        //Map<Character, Integer> mp = new HashMap<>();
-        Map<Character, Integer> map =new HashMap<>();
 
-        // Step 2: Traverse the string and count frequency of each character
-        // for (char a : s.toCharArray()) {
-        //     mp.put(a, mp.getOrDefault(a, 0) + 1); // Increment frequency
-        // }
-        for(char a: s.toCharArray()){
-          map.put(a, map.getOrDefault(a, 0)+1);
+      Map<Character, Integer> map = new HashMap<>();
+
+      for( char ch : s.toCharArray()){
+        map.put(ch, getOrDefault(ch, 0)+1);
+      }
+      for(int i=0; i<s.length(); i++){
+        if(map.put(s.charAt(i))==1){
+          return i;
+
         }
-
-        // Step 3: Traverse the string again to find the first character with frequency 1
-        for (int i = 0; i < s.length(); i++) {
-            if (map.get(s.charAt(i)) == 1) {
-                return i; // Return the index of first unique character
-            }
-        }
-
-        // Step 4: If no unique character found, return -1
+      }
         return -1;
     }
 }
