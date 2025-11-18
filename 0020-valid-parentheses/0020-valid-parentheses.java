130bd5b2c7a15cde512c49c1567ce4ac60294950
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-11-18 13:50:25 +0530

    Time: 2 ms (97.7%), Space: 43.3 MB (16.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,22 +4,13 @@ class Solution {
       for( char c : s.toCharArray()){
         if(c =='(')
         stack.push(')');
-
         else if(c == '{')
         stack.push('}');
-
         else if( c== '[')
         stack.push(']');
-
         else if( stack.isEmpty() || stack.pop()!= c)
-          return false;
-        
+          return false; 
       }
-        return stack.isEmpty();
-
-      
-     
-
-        
+        return stack.isEmpty();    
     }
 }
