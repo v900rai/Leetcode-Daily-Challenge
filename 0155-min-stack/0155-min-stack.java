315d6c09d06a11dd8527324868dc999a81728dff
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-11-19 16:09:02 +0530

    Time: 5 ms (51.64%), Space: 47.1 MB (17.27%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -23,3 +23,13 @@ class MinStack {
         return minStack.peek();
     }
 }
+
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
