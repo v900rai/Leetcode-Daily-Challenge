6d23ca7fbae6508e20d6cfbcf2e3eaea0bbf5b0f
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-11-23 11:00:46 +0530

    Time: 21 ms (9.63%), Space: 53.2 MB (10.25%) - LeetHub

---
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int maxSumDivThree(int[] nums) {
+        int sum=0;
+        List<Integer>list1=new ArrayList<>();
+        List<Integer>list2=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%3==1){
+                list1.add(nums[i]);
+            }
+            else if(nums[i]%3==2){
+                list2.add(nums[i]);
+            }
+            sum+=nums[i];
+        }
+        if(sum%3==0)return sum;
+        Collections.sort(list1);
+        Collections.sort(list2);
+        if(sum%3==1){
+           if(list1.size()>0&&list2.size()>1) {
+            sum-=Math.min(list1.get(0),list2.get(0)+list2.get(1));
+            }
+            else if(list1.size()>0){
+                sum-=list1.get(0);
+            }
+            else{sum-=(list2.get(0)+list2.get(1));}
+        }
+        else if(sum%3==2){
+            if(list2.size()>0 && list1.size()>1){
+                sum-=Math.min(list2.get(0),(list1.get(0)+list1.get(1)));
+            }
+            else if(list2.size()>0){
+                sum-=list2.get(0);
+            }
+            else{
+                sum-=(list1.get(0)+list1.get(1));
+            }
+        }
+        return sum;
+
+    }
+}
