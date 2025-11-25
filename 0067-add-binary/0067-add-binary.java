9973a1e330efb55e388e50e7a7486df23ff6c9e6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-11-25 16:17:05 +0530

    Time: 1 ms (99.81%), Space: 43.7 MB (15.88%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,19 +1,28 @@
 class Solution {
     public String addBinary(String a, String b) {
+
         StringBuilder res = new StringBuilder();
-        int i = a.length() - 1;
-        int j = b.length() - 1;
+
+        int i = a.length() - 1; // last index of a
+        int j = b.length() - 1; // last index of b
         int carry = 0;
-        while(i >= 0 || j >= 0){
-            int sum = carry;
+
+        while(i >= 0 || j >= 0){  // jab tak dono complete na ho jaye
+            int sum = carry;     // start sum with carry
+
             if(i >= 0)
-             sum += a.charAt(i--) - '0';
+                sum += a.charAt(i--) - '0'; // char ko number me convert
             if(j >= 0)
-            sum += b.charAt(j--) - '0';
-            carry = sum > 1 ? 1 : 0;
-            res.append(sum % 2);
+                sum += b.charAt(j--) - '0';
+
+            carry = sum > 1 ? 1 : 0; // agar sum 2 or 3 ho → carry = 1
+
+            res.append(sum % 2); // 0 ya 1 add hoga
         }
-        if(carry != 0) res.append(carry);
-        return res.reverse().toString();
+
+        if(carry != 0)
+            res.append(carry); // last me carry bach jaye to add
+
+        return res.reverse().toString(); // because LSB se add kiya
     }
 }
