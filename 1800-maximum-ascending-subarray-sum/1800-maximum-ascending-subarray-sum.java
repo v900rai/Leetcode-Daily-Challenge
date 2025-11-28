2b459ba4be4cb6755eafc4789f124aec26071a25
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-11-28 17:43:45 +0530

    Time: 0 ms (100%), Space: 43.1 MB (5.05%) - LeetHub

--- 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.java
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.java
@@ -1,19 +1,18 @@
 class Solution {
     public int maxAscendingSum(int[] nums) {
-        // initialize the current sum and maxsum
-        int currentSum=nums[0];
-        int maxSum=currentSum;
-        for(int i=1; i<nums.length; i++){
-            if(nums[i]>nums[i-1]){
-                currentSum=currentSum+nums[i];
-            }
-            else {
-                // start the new sub arrays 
-                currentSum=nums[i];
-            }
-            maxSum=Math.max(maxSum, currentSum);
-        }
-        return maxSum;
+      int sum = nums[0];
+      int maxSum = nums[0];
+      for(int i=1; i<nums.length; i++){
+        if(nums[i]>nums[i-1])
+        sum = sum+nums[i];
+        else
+          sum = nums[i];
+        
+        if(sum> maxSum)
+          maxSum  = sum;
+        
+      }
+      return maxSum;
         
     }
 }
