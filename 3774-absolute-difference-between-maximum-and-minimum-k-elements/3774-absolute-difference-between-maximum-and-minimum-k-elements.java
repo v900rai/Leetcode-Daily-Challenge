06be50723e68fe6dc7f3ea697be395998a050ece
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-12-14 10:32:35 +0530

    Time: 5 ms (100%), Space: 46.8 MB (100%) - LeetHub

--- 3774-absolute-difference-between-maximum-and-minimum-k-elements/3774-absolute-difference-between-maximum-and-minimum-k-elements.java
+++ 3774-absolute-difference-between-maximum-and-minimum-k-elements/3774-absolute-difference-between-maximum-and-minimum-k-elements.java
@@ -7,7 +7,8 @@ class Solution {
         int sum1 = 0;
         int sum2 = 0;
         for(int i=n-1;i>=n-k;i--){
-            sum1 += arr[i];
+            
+            sum1=sum1+arr[i];
         }
         for(int i=0;i<k;i++){
             sum2 += arr[i];
