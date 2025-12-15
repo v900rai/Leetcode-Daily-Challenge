f6a71c6f8a8b7b5ec62c16ac4ef75a88ed55e8a9
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-12-15 10:38:29 +0530

    Time: 5 ms (77.19%), Space: 76.9 MB (67.14%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,23 +1,42 @@
 class Solution {
+
+    // This method finds the maximum water that can be contained
+    // using the two-pointer approach
     public int maxArea(int[] height) {
+
+        // Left pointer at start of array
         int left = 0;
-        int right = height.length-1;
-        int ans = 0; 
-        while(left < right){
+
+        // Right pointer at end of array
+        int right = height.length - 1;
+
+        // Variable to store maximum area
+        int ans = 0;
+
+        // Continue until both pointers meet
+        while (left < right) {
+
+            // Width between the two lines
             int w = right - left;
-           
+
+            // Height is the minimum of two lines
             int h = Math.min(height[left], height[right]);
+
+            // Calculate current area
             int area = w * h;
-            ans = Math.max(area, ans);  // Fixed: using max instead of min
-             
+
+            // Store the maximum area
+            ans = Math.max(ans, area);
+
+            // Move the pointer with smaller height
             if (height[left] < height[right]) {
                 left++;
             } else {
                 right--;
             }
         }
+
+        // Return the maximum water area
         return ans;
     }
 }
-
-
