bb16b85d327b170f2e5f6df62535f7a80f7fb7cb
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-12-19 11:17:48 +0530

    Time: 2 ms (95.73%), Space: 48.6 MB (13.99%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,40 +1,47 @@
 class Solution {
-
-    // This method finds two numbers in a sorted array
-    // whose sum is equal to the given target
     public int[] twoSum(int[] numbers, int target) {
 
-        // Pointer starting from the beginning of the array
-        int left = 0;
-
-        // Pointer starting from the end of the array
-        int right = numbers.length - 1;
-
-        // Loop continues until both pointers meet
-        while (left < right) {
-
-            // Calculate sum of elements at left and right pointers
-            int sum = numbers[left] + numbers[right];
-
-            // If the sum matches the target, return indices (1-based)
-            if (sum == target) {
-                return new int[]{left + 1, right + 1};
-            }
-
-            // If sum is smaller than target,
-            // move left pointer to the right to increase sum
-            else if (sum < target) {
-                left++;
-            }
-
-            // If sum is greater than target,
-            // move right pointer to the left to decrease sum
-            else {
-                right--;
-            }
+      int left =0;
+      int right = numbers.length-1;
+      while(left <right){
+        int sum = numbers[left] + numbers[right];
+        if(sum == target){
+          return new int[] {left+1 , right+1};
         }
-
-        // If no valid pair is found, return -1, -1
-        return new int[]{-1, -1};
+        if(sum<target){
+          left++;
+        }
+        else{
+          right--;
+        }
+      }
+        return new int[] {-1,-1};
+      }
     }
-}
+
+//         int left = 0;                     // Start pointer
+//         int right = numbers.length - 1;   // End pointer
+
+//         while (left < right) {
+
+//             int sum = numbers[left] + numbers[right];
+
+//             // If sum matches target
+//             if (sum == target) {
+//                 // Return 1-based index
+//                 return new int[]{left + 1, right + 1};
+//             }
+//             // If sum is smaller, move left pointer
+//             else if (sum < target) {
+//                 left++;
+//             }
+//             // If sum is greater, move right pointer
+//             else {
+//                 right--;
+//             }
+//         }
+
+//         // Problem guarantee solution exists
+//         return new int[]{-1, -1};
+//     }
+// }
