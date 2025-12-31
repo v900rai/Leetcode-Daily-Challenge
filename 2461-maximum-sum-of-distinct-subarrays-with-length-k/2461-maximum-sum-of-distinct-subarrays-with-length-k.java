4f77fb16a7873b9db6a55bfc400ba79e69e75027
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-12-31 10:51:11 +0530

    Time: 33 ms (89.39%), Space: 98.8 MB (47.47%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,20 +1,36 @@
 class Solution {
-    public long maximumSubarraySum(int[] arr, int k) {
-      
-    
-        int windowSum = 0;
-        int maxSum = 0;
+    public long maximumSubarraySum(int[] nums, int k) {
+       int i = 0, j = 0;
+        long result = 0;
+        long currWindowSum = 0;
+        HashSet<Integer> set = new HashSet<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            windowSum += arr[i];          // add right element
-
-            if (i >= k - 1) {             // window size k reached
-                maxSum = Math.max(maxSum, windowSum);
-                windowSum -= arr[i - (k - 1)]; // remove left element
+        while (j < nums.length) {
+            // Adjust the window if nums[j] is already in the set
+            while (set.contains(nums[j])) {
+                currWindowSum -= nums[i];
+                set.remove(nums[i]);
+                i++;
             }
-        }
-        return maxSum;
-    }
-}
 
+            currWindowSum += nums[j];
+            set.add(nums[j]);
+
+            // Check if the window size is equal to k
+            if (j - i + 1 == k) {
+                result = Math.max(result, currWindowSum);
+
+                // Shrink the window from the left
+                currWindowSum -= nums[i];
+                set.remove(nums[i]);
+                i++;
+            }
+
+            j++;
+        }
+
+        return result;
+    }
+ 
     
+}
