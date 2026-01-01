eec782323492e408629d35593ea8c39878c5cbb5
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-01 15:17:34 +0530

    Time: 1 ms (97.8%), Space: 47.2 MB (74.23%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,32 +1,21 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+      int[] pre = new int [nums.length];
+      pre[0] = 0;
+      int sum = nums[0];
 
-        int n = nums.length;
+      for(int i=1; i<nums.length; i++){
+        sum =  sum+nums[i];
 
-        int[] pre = new int[n];
-        pre[0] = 0; // 0th index ke left me kuch nhi hai
-
-        // Step 1: Create prefix sum array
-        for (int i = 1; i < n; i++) {
-            pre[i] = pre[i - 1] + nums[i - 1];
+        pre[i] = pre[i-1] + nums[i-1];
+      }
+      for(int i=0; i<nums.length; i++){
+        if(pre[i] ==sum -nums[i]-pre[i]){
+          return i;
         }
+      }
+      return -1;
 
-        // Step 2: Calculate total sum
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
-
-        // Step 3: Check pivot index
-        for (int i = 0; i < n; i++) {
-            int leftSum = pre[i];
-            int rightSum = totalSum - nums[i] - leftSum;
-
-            if (leftSum == rightSum) {
-                return i;
-            }
-        }
-
-        return -1; // No pivot index found
+        
     }
 }
