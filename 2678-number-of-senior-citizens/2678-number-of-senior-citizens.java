d793b0d1ad4fcbefe265a89eb68d94f231cc1501
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-02 17:46:20 +0530

    Time: 1 ms (75.7%), Space: 44.8 MB (8.57%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -0,0 +1,13 @@
+
+
+class Solution {
+    public int countSeniors(String[] details) {
+        int count=0;
+        for(String i : details){
+            String s=i.substring(11,13);
+            int age=Integer.parseInt(i.substring(11,13));
+            if(age>60) count++;
+        }
+        return count;
+    }
+}
