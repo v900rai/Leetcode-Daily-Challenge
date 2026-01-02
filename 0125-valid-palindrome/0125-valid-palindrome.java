7490a223616a5678151c01421556ecf2a09666a0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-02 10:55:56 +0530

    Time: 2 ms (98.88%), Space: 44.2 MB (76.85%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,40 +1,69 @@
 
 class Solution {
   public boolean isPalindrome(String s) {
-    int start = 0; // Pointer at the beginning of the string
-    int end = s.length() - 1; // Pointer at the end of the string
 
-    while (start < end) {
-      // Skip non-alphanumeric characters from the left
-      
-    
-     
-      while(start< end && !Character.isLetterOrDigit(s.charAt(start))){
+    int start = 0;
+    int end = s.length()-1;
+
+    while(start <end){
+      while(start <end && !Character.isLetterOrDigit(s.charAt(start))){
         start++;
       }
-
-      // Skip non-alphanumeric characters from the right
-      while (start < end && !Character.isLetterOrDigit(s.charAt(end))) {
+      while(start <end && !Character.isLetterOrDigit(s.charAt(end))){
         end--;
       }
-
-      // Convert both characters to lowercase and compare
-
       char startchar = Character.toLowerCase(s.charAt(start));
       
       char endchar = Character.toLowerCase(s.charAt(end));
-
-      // If characters don't match, it's not a palindrome
       if (startchar != endchar) {
         return false;
       }
-
-      // Move both pointers towards the center
+       // Move both pointers towards the center
       start++;
       end--;
     }
 
     // If all characters matched, it's a palindrome
     return true;
-  }
+
+    }
 }
+
+
+//     int start = 0; // Pointer at the beginning of the string
+//     int end = s.length() - 1; // Pointer at the end of the string
+
+//     while (start < end) {
+//       // Skip non-alphanumeric characters from the left
+      
+    
+     
+//       while(start< end && !Character.isLetterOrDigit(s.charAt(start))){
+//         start++;
+//       }
+
+//       // Skip non-alphanumeric characters from the right
+//       while (start < end && !Character.isLetterOrDigit(s.charAt(end))) {
+//         end--;
+//       }
+
+//       // Convert both characters to lowercase and compare
+
+//       char startchar = Character.toLowerCase(s.charAt(start));
+      
+//       char endchar = Character.toLowerCase(s.charAt(end));
+
+//       // If characters don't match, it's not a palindrome
+//       if (startchar != endchar) {
+//         return false;
+//       }
+
+//       // Move both pointers towards the center
+//       start++;
+//       end--;
+//     }
+
+//     // If all characters matched, it's a palindrome
+//     return true;
+//   }
+// }
