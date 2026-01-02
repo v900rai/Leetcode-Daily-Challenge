4692c31b6793a86ae8b9be8c5182922d502f7746
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-02 19:38:02 +0530

    Time: 0 ms (100%), Space: 45.2 MB (30.79%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.java
+++ 3151-special-array-i/3151-special-array-i.java
@@ -1,16 +1,16 @@
 class Solution {
-    public boolean isArraySpecial(int[] nums) {
+  public boolean isArraySpecial(int[] nums) {
 
-        if(nums.length == 1)
-         return true;
-        
-        for(int i=1; i<nums.length; i++){
-            if(nums[i]%2 == nums[i-1]%2)
-             return false;
-            
-        }
-        
-        return true;
-        
+    if (nums.length == 1)
+      return true;
+
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] % 2 == nums[i - 1] % 2)
+        return false;
+
     }
+
+    return true;
+
+  }
 }
