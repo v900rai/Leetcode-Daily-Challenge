3487692793bae866a92e192a68277e4e36557b1e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-08 13:43:16 +0530

    Time: 72 ms (1.62%), Space: 43.4 MB (54.67%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.java
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -1,13 +1,12 @@
 class Solution {
     public int scoreOfString(String s) {
-      int score=0;
-      int n=s.length()-1;
-      for(int i=0; i<n; i++){
-        if(s.charAt(i)!=s.charAt(i+1))
-        score=score+Math.abs(s.charAt(i)-s.charAt(i+1));
-      }
-      return score;
-
-        
+        int ans =0;
+        int n = s.length()-1; 
+        for(int i=0; i<n; i++){
+          if(s.charAt(i) != s.charAt(i+1)){
+            ans =ans+Math.abs(s.charAt(i) -s.charAt(i+1));
+          }
+        }
+        return ans;
     }
 }
