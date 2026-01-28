7266e9160f475fe3f2107a2dfd52c7b09fe8cf49
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-28 12:07:01 +0530

    Time: 16 ms (32.95%), Space: 46.6 MB (33.65%) - LeetHub

--- 1895-largest-magic-square/1895-largest-magic-square.java
+++ 1895-largest-magic-square/1895-largest-magic-square.java
@@ -1,60 +1,63 @@
 class Solution {
-    int [][] rowPref, colPref, diag1Pref, diag2Pref;
-    public int largestMagicSquare(int[][] grid) {
+  int[][] rowPref, colPref, diag1Pref, diag2Pref;
 
-        int m=grid.length, n=grid[0].length;
+  public int largestMagicSquare(int[][] grid) {
 
-        buildPrefix(grid);
+    int m = grid.length;
+    int n = grid[0].length;
 
-        int max=1;
-        for(int len=2; len <=Math.min(m,n);len++){
-            for(int row=0; row+len <=m;row++){
-                for(int col=0; col+len <=n;col++){
-                    if(checkMagic(row,col,len)){
-                        max=Math.max(max,len);
-                    }
-                }
-            }
+    buildPrefix(grid);
+
+    int max = 1;
+    for (int len = 2; len <= Math.min(m, n); len++) {
+      for (int row = 0; row + len <= m; row++) {
+        for (int col = 0; col + len <= n; col++) {
+          if (checkMagic(row, col, len)) {
+            max = Math.max(max, len);
+          }
         }
-        return max;
-        
+      }
     }
+    return max;
 
-    void buildPrefix(int [][] grid){
-        int m=grid.length;
-        int n=grid[0].length;
+  }
 
-        rowPref=new int[m][n+1];
-        colPref= new int [m+1][n];
-        diag1Pref=new int[m+1][n+1];
-        diag2Pref=new int[m+1][n+1];
+  void buildPrefix(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
 
-        for(int i=0; i<m;i++){
-            for(int j=0;j<n;j++){
-                rowPref[i][j+1]=rowPref[i][j] + grid[i][j];
-                colPref[i+1][j]=colPref[i][j] +grid[i][j];
-                diag1Pref[i+1][j+1]=diag1Pref[i][j] + grid[i][j];
-                diag2Pref[i+1][j]= diag2Pref[i][j+1] + grid[i][j];
-            }
-        }
+    rowPref = new int[m][n + 1];
+    colPref = new int[m + 1][n];
+    diag1Pref = new int[m + 1][n + 1];
+    diag2Pref = new int[m + 1][n + 1];
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        rowPref[i][j + 1] = rowPref[i][j] + grid[i][j];
+        colPref[i + 1][j] = colPref[i][j] + grid[i][j];
+        diag1Pref[i + 1][j + 1] = diag1Pref[i][j] + grid[i][j];
+        diag2Pref[i + 1][j] = diag2Pref[i][j + 1] + grid[i][j];
+      }
     }
-    boolean checkMagic(int row,int col, int side){
-        int  target=rowPref[row][col+side]-rowPref[row][col];
+  }
 
-        for(int i=row; i<row+side;i++){
-            if(rowPref[i][col+side] - rowPref[i][col] !=target)
-            return false;
-        }
-        for(int j=col;j< col+side;j++){
-            if(colPref[row +side] [j] - colPref[row][j] != target)
-            return false;
-        }
-        if(diag1Pref[row+side][col+side] - diag1Pref[row][col] != target)
+  boolean checkMagic(int row, int col, int side) {
+    int target = rowPref[row][col + side] - rowPref[row][col];
+
+    for (int i = row; i < row + side; i++) {
+      if (rowPref[i][col + side] - rowPref[i][col] != target)
         return false;
-
-        if(diag2Pref[row + side][col] - diag2Pref[row][col + side] !=target)
-        return false;
-
-        return true;
     }
+    for (int j = col; j < col + side; j++) {
+      if (colPref[row + side][j] - colPref[row][j] != target)
+        return false;
+    }
+    if (diag1Pref[row + side][col + side] - diag1Pref[row][col] != target)
+      return false;
+
+    if (diag2Pref[row + side][col] - diag2Pref[row][col + side] != target)
+      return false;
+
+    return true;
+  }
 }
