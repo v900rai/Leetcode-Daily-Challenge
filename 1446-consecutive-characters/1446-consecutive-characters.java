e72af8d68e922fa93d9c77c1d04123105e5f786e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-02 18:22:29 +0530

    Time: 1 ms (100%), Space: 43.7 MB (22.91%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.java
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -1,22 +1,16 @@
 class Solution {
-  public int maxPower(String s) {
-    int maxCharacter = 1;
-    int len = 1;
-    for (int i = 1; i < s.length(); i++) {
-      if (s.charAt(i) == s.charAt(i-1)) {
-        len++;
-      } else {
-        if (maxCharacter < len) {
-          maxCharacter= len;
+    public int maxPower(String s) {
+        int ans = 1;
+        int count = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                count = 1;
+            }
+            ans = Math.max(ans, count);
         }
-        len = 1;
-      }
+        return ans;
     }
-    if (maxCharacter < len) {
-      maxCharacter = len;
-    }
-
-    return maxCharacter;
-
-  }
 }
