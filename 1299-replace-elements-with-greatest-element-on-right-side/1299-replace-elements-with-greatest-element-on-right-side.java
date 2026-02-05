5dc043e1170049d086eb9849229f58c62bd2bf71
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-05 17:07:34 +0530

    Time: 1417 ms (5.24%), Space: 49.3 MB (30.15%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,36 +1,25 @@
 class Solution {
 
-  // Function to replace every element with the greatest element
-  // among the elements to its right side
-  public int[] replaceElements(int[] arr) {
+    public int[] replaceElements(int[] arr) {
 
-    // max variable stores the maximum element seen so far from the right
-    // Initially -1 because last element ka answer hamesha -1 hota hai
-    int max = -1;
+        int n = arr.length;
 
-    // Traverse the array from right to left
-    for (int i = arr.length - 1; i >= 0; i--) {
+        // Loop for every element
+        for (int i = 0; i < n; i++) {
 
-      // If current element is greater than max
-      if (arr[i] > max) {
+            int max = -1;
 
-        // temp stores the old max value
-        int temp = max;
+            // Find max element on right side
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] > max) {
+                    max = arr[j];
+                }
+            }
 
-        // Update max with current element
-        max = arr[i];
+            // Replace current element
+            arr[i] = max;
+        }
 
-        // Replace current element with previous max
-        arr[i] = temp;
-
-      } else {
-        // If current element is smaller than max
-        // Replace it directly with max
-        arr[i] = max;
-      }
+        return arr;
     }
-
-    // Return the modified array
-    return arr;
-  }
 }
