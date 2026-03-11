8b46b151d732d6407403b4f339c0b659ec85a8f7
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-03-11 10:11:45 +0530

    Time: 0 ms (100%), Space: 42.3 MB (32.44%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        if (n == 0)
+            return 1;
+
+        int mask = 1;
+        while (mask < n) {
+            mask = (mask << 1) | 1;  // builds 111...1
+        }
+        return n ^ mask;
+    }
+}
