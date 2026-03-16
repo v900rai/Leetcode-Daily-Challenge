2c446c403e8d991f1b1e88ce73ff017cdcf11254
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-03-16 10:54:44 +0530

    Time: 2 ms (69.94%), Space: 42.4 MB (97.22%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,17 +1,15 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-        int i = 0; // s ke liye pointer
-        int j = 0; // t ke liye pointer
+  public boolean isSubsequence(String s, String t) {
 
-        while (i < s.length() && j < t.length()) {
-            // agar dono characters same hain
-            if (s.charAt(i) == t.charAt(j)) {
-                i++;
-            }
-            j++;
+      int i = 0;
+      int j = 0;
+      while(i< s.length() && j<t.length()){
+        if(s.charAt(i) ==t. charAt(j)){
+          i++;
         }
-
-        // agar s ke saare characters match ho gaye
-        return i == s.length();
+        j++;
+      }
+      return i == s.length();
+        
     }
 }
