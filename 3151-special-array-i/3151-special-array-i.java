ce96908286b74a7e60e778899a09b1b968b1478b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-03-16 11:05:11 +0530

    Time: 1 ms (64.09%), Space: 44.8 MB (96.7%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.java
+++ 3151-special-array-i/3151-special-array-i.java
@@ -1,16 +1,20 @@
 class Solution {
-  public boolean isArraySpecial(int[] nums) {
+    public boolean isArraySpecial(int[] nums) {
 
-    if (nums.length == 1)
-      return true;
+        // Agar array me sirf 1 element hai to wo automatically special hoga
+        if (nums.length == 1)
+            return true;
 
-    for (int i = 1; i < nums.length; i++) {
-      if (nums[i] % 2 == nums[i - 1] % 2)
-        return false;
+        // Adjacent elements ka parity check karenge
+        for (int i = 1; i < nums.length; i++) {
 
+            // Agar current aur previous dono even ya dono odd hue
+            // to array special nahi hai
+            if (nums[i] % 2 == nums[i - 1] % 2)
+                return false;
+        }
+
+        // Agar loop complete ho gaya, matlab sab adjacent pairs alternate parity me hain
+        return true;
     }
-
-    return true;
-
-  }
 }
