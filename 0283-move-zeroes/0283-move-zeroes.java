a71f9cbde1778fd128792cfaac480c5a766abae2
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-03-17 10:10:32 +0530

    Time: 2 ms (90.52%), Space: 47.6 MB (92.06%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,8 +3,6 @@ class Solution {
         int left=0;
         for(int right=0; right<nums.length; right++){
            if(nums[right] != 0){
-               
-
                int temp = nums[right];
                nums[right] = nums[left];
                nums[left] = temp;
