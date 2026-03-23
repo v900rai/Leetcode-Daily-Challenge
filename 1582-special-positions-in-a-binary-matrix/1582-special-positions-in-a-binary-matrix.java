3511f59249b1ac9bf52828dccb53e671c7467f61
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-03-23 16:20:35 +0530

    Time: 2 ms (95.9%), Space: 47 MB (65.45%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -17,12 +17,11 @@ class Solution {
                 }
             }
         }
-
         int ans = 0;
-
         for (int i = 0; i < m; i++) 
             for (int j = 0; j < n; j++) 
-                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1) ans++;
+                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1)
+                 ans++;
 
         return ans;
     }
