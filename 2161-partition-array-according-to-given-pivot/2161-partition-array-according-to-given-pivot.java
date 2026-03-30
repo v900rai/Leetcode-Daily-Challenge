5be0aade549dd9a17f9745b74eb9963fe19bfac7
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-03-30 11:23:33 +0530

    Time: 5 ms (72.38%), Space: 160.1 MB (29.78%) - LeetHub

---
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -0,0 +1,28 @@
+// Time complexity = 2n = O(n)
+// Space complexity = O(1), or O(n) if the result array is including in the complexity analysis.
+
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int[] result = new int[nums.length];
+        int left = 0;
+        int right = nums.length - 1;
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] < pivot) {
+                result[left] = nums[i];
+                left++;
+            }
+            if(nums[nums.length - 1 - i] > pivot) {
+                result[right] = nums[nums.length - 1 - i];
+                right--;
+            }
+        }
+        
+        while(left <= right) {
+            result[left++] = pivot;
+            result[right--] = pivot;
+        }
+        
+        return result;
+    } 
+}
