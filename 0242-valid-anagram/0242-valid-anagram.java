a9007b9ce8824fa8900411f3fc86d5af02f5f7c2
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-04-01 19:48:19 +0530

    Time: 4 ms (81.87%), Space: 46.3 MB (43.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,12 @@
 class Solution {
   public boolean isAnagram(String s, String t) {
-    if (s.length() != t.length()) {
+    if(s.length() != t.length()){
       return false;
     }
     char[] sarr = s.toCharArray();
     char[] tarr = t.toCharArray();
     Arrays.sort(sarr);
     Arrays.sort(tarr);
-    // Compare the sorted arrays
-    //return Arrays.equals(sarr, tarr);
     return Arrays.equals(sarr, tarr);
 
   }
