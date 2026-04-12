48c1dc11881de976533a8452ddce966e7da580f9
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-04-12 15:59:15 +0530

    Time: 20 ms (83.41%), Space: 56.4 MB (81.6%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -6,7 +6,9 @@ class Solution {
       int left = 0;
       int right = people.length-1;
       while(left <=right){
-        if(people[left] + people[right]<=limit){
+       
+        
+        if(people[left] + people[right] <= limit){
           left++;
         }
         right--;
