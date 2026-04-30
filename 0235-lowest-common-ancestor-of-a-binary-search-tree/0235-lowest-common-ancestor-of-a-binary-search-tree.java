2f34059b676e004fa15d032f81100612225fca93
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-04-30 16:25:11 +0530

    Time: 6 ms (97.34%), Space: 48 MB (17.11%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -5,24 +5,41 @@
  *     TreeNode left;
  *     TreeNode right;
  *     TreeNode(int x) { 
-  val = x; 
-  }
+ *         val = x; 
+ *     }
  * }
  */
+
 class Solution {
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        // Traverse the tree starting from root
         while (root != null) {
-            if(p.val <root.val && q.val<root.val){
-              root = root.left;
+
+            // Case 1: If both p and q are smaller than root
+            // Hindi: agar p aur q dono root se chhote hain
+            // then LCA will be in left subtree
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left; // move left
             }
-            else if(p.val>root.val && q.val>root.val){
-              root =root.right;
+
+            // Case 2: If both p and q are greater than root
+            // Hindi: agar p aur q dono root se bade hain
+            // then LCA will be in right subtree
+            else if (p.val > root.val && q.val > root.val) {
+                root = root.right; // move right
             }
+
+            // Case 3: We found the split point
+            // Hindi: ek left me hai aur ek right me (ya root hi LCA hai)
+            // This is the Lowest Common Ancestor
             else {
-                return root; 
+                return root;
             }
         }
-        
+
+        // If tree is empty or no LCA found
         return null;
     }
 }
