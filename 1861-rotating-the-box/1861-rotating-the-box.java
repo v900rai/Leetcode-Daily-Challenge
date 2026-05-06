db3449c1ccff2f1bb747fa524a0674ba9493c7b2
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-05-06 10:41:42 +0530

    Time: 322 ms (5.05%), Space: 125 MB (13.46%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.java
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -1,25 +1,60 @@
 class Solution {
     public char[][] rotateTheBox(char[][] box) {
+
+        // Number of rows
         int row = box.length;
+
+        // Number of columns
         int col = box[0].length;
+
+        // New matrix after rotation
         char[][] box_90 = new char[col][row];
 
-        // Simulate gravity
+        // -----------------------------
+        // STEP 1 : Simulate Gravity
+        // -----------------------------
+
         for (int i = 0; i < row; i++) {
+
+            // cell points to the position
+            // where next stone '#' should fall
             int cell = col - 1;
+
+            // Traverse row from right to left
             for (int j = col - 1; j >= 0; j--) {
+
+                // If obstacle found
                 if (box[i][j] == '*') {
+
+                    // Stone cannot cross obstacle
+                    // So next available cell becomes left side
                     cell = j - 1;
-                } else if (box[i][j] == '#') {
+                }
+
+                // If stone found
+                else if (box[i][j] == '#') {
+
+                    // Remove current stone
                     box[i][j] = '.';
-                    box[i][cell--] = '#';
+
+                    // Place stone at correct falling position
+                    box[i][cell] = '#';
+
+                    // Move cell left
+                    cell--;
                 }
             }
         }
 
-        // Rotate the box 90 degrees clockwise
+        // -----------------------------
+        // STEP 2 : Rotate 90 Degree
+        // -----------------------------
+
         for (int i = 0; i < row; i++) {
+
             for (int j = 0; j < col; j++) {
+
+                // Rotate clockwise
                 box_90[j][row - i - 1] = box[i][j];
             }
         }
