53fd8f958f39eaa6820dd7ba5a73bba355d467c3
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-05-30 09:55:17 +0530

    Time: 1 ms (96.59%), Space: 47.4 MB (16.7%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -1,15 +1,21 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
 
-      //8888fadfgqhuq
-      int ans[] = new int[2 * nums.length]; 
-      for(int i=0; i<nums.length; i++ ){
-        ans[i] = nums[i];
-        ans[i + nums.length] = nums[i];
+        // Create a new array of double size
+        // क्योंकि हमें original array को 2 बार store करना है
+        int ans[] = new int[2 * nums.length];
 
-      }
-      return ans;
+        // Loop through original array
+        for(int i = 0; i < nums.length; i++) {
 
+            // Copy element at same index
+            ans[i] = nums[i];
+
+            // Copy same element again after nums.length index
+            ans[i + nums.length] = nums[i];
+        }
+
+        // Return final concatenated array
+        return ans;
     }
-
 }
