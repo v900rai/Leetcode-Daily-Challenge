62c21cee44a327fe197362902b87ad33eabc3d02
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-05-30 12:11:57 +0530

    Time: 1398 ms (5.9%), Space: 49 MB (79.75%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -4,22 +4,26 @@ class Solution {
 
         int n = arr.length;
 
-        // Loop for every element
+        // Traverse each element of array
         for (int i = 0; i < n; i++) {
 
+            // Assume maximum element on right side is -1
             int max = -1;
 
-            // Find max element on right side
+            // Find the maximum element on the right side of current index
             for (int j = i + 1; j < n; j++) {
+
+                // Update max if bigger element is found
                 if (arr[j] > max) {
                     max = arr[j];
                 }
             }
 
-            // Replace current element
+            // Replace current element with right side maximum
             arr[i] = max;
         }
 
+        // Return modified array
         return arr;
     }
 }
