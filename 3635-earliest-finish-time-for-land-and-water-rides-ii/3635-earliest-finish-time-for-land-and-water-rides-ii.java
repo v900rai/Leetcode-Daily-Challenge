5d4cadafa79de84e650c1229202676f61cf13c10
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-06-03 14:51:27 +0530

    Time: 2 ms (100%), Space: 91.8 MB (84.48%) - LeetHub

--- 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.java
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.java
@@ -1,14 +1,13 @@
 class Solution {
     private int calFinishTime(int[] ls, int[] ld,int[] ws, int[] wd) {
-
+        
+        
         int mini = Integer.MAX_VALUE;
-
         for (int i = 0; i < ls.length; i++) {
             mini = Math.min(mini, ls[i] + ld[i]);
         }
 
         int ans = Integer.MAX_VALUE;
-
         for (int i = 0; i < ws.length; i++) {
             ans = Math.min(
                 ans,
