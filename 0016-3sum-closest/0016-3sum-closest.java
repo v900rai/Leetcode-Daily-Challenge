c741d8dc669088528085388a979fd7b120f66cd4
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-06-08 22:04:20 +0530

    Time: 701 ms (5%), Space: 45.1 MB (92.41%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,19 +1,33 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
+
         int len = nums.length;
-        int sum =0;
-        int closestSum = nums[0] + nums[1] + nums[2]; 
-        for(int i=0; i<len-2; i++){
-            for(int j=i+1; j<len-1; j++){
-                for(int k=j+1; k<len;  k++){
-                    sum = (nums[i]+nums[j]+nums[k]);
-                    //System.out.println(nums[i]+" : "+nums[j]+" : "+nums[k]+" : "+sum);
-                     if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+
+        int sum = 0;
+
+        // First 3 elements ka sum initial closest answer maan liya
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        // First element choose kar rahe hain
+        for (int i = 0; i < len - 2; i++) {
+
+            // Second element choose kar rahe hain
+            for (int j = i + 1; j < len - 1; j++) {
+
+                // Third element choose kar rahe hain
+                for (int k = j + 1; k < len; k++) {
+
+                    // Current triplet ka sum
+                    sum = nums[i] + nums[j] + nums[k];
+
+                    // Agar current sum target ke aur paas hai
+                    if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
                         closestSum = sum;
                     }
                 }
             }
         }
+
         return closestSum;
     }
 }
