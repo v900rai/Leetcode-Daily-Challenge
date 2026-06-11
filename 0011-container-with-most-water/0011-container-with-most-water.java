8cda536c965c2de7cf6a65e788faf2565b8ea07e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-06-11 14:00:43 +0530

    Time: 5 ms (82.67%), Space: 77.7 MB (11.79%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,41 +1,41 @@
 class Solution {
-    // This method finds the maximum water that can be contained
-    // using the two-pointer approach
-    public int maxArea(int[] height) {
+  // This method finds the maximum water that can be contained
+  // using the two-pointer approach
+  public int maxArea(int[] height) {
 
-        // Left pointer at start of array
-        int left = 0;
+    // Left pointer at start of array
+    int left = 0;
 
-        // Right pointer at end of array
-        int right = height.length - 1;
+    // Right pointer at end of array
+    int right = height.length - 1;
 
-        // Variable to store maximum area
-        int ans = 0;
+    // Variable to store maximum area
+    int ans = 0;
 
-        // Continue until both pointers meet
-        while (left < right) {
+    // Continue until both pointers meet
+    while (left < right) {
 
-            // Width between the two lines
-            int w = right - left;
+      // Width between the two lines
+      int w = right - left;
 
-            // Height is the minimum of two lines
-            
-            int h = Math.min(height[left], height[right]);
+      // Height is the minimum of two lines
 
-            // Calculate current area
-            int area = w * h;
+      int h = Math.min(height[left], height[right]);
 
-            // Store the maximum area
-            ans = Math.max(ans, area);
+      // Calculate current area
+      int area = w * h;
 
-            // Move the pointer with smaller height
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-        // Return the maximum water area
-        return ans;
+      // Store the maximum area
+      ans = Math.max(ans, area);
+
+      // Move the pointer with smaller height
+      if (height[left] < height[right]) {
+        left++;
+      } else {
+        right--;
+      }
     }
+    // Return the maximum water area
+    return ans;
+  }
 }
