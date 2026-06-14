c18872a989c2c02b93eac89da85a031edebf07c4
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-06-14 23:25:28 +0530

    Time: 4 ms (98.3%), Space: 102 MB (35.98%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -9,27 +9,29 @@
  * }
  */
 class Solution {
-    public int pairSum(ListNode head) {
-        ListNode slow = head, fast = head, prev = null;
+  public int pairSum(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    ListNode prev = null;
 
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-
-            ListNode nxt = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = nxt;
-        }
-
-        int ans = 0;
-        ListNode left = prev, right = slow;
-
-        while (left != null) {
-            ans = Math.max(ans, left.val + right.val);
-            left = left.next;
-            right = right.next;
-        }
-
-        return ans;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      ListNode nxt = slow.next;
+      slow.next = prev;
+      prev = slow;
+      slow = nxt;
     }
+
+    int ans = 0;
+    ListNode left = prev;
+    ListNode right = slow;
+
+    while (left != null) {
+      ans = Math.max(ans, left.val + right.val);
+      left = left.next;
+      right = right.next;
+    }
+
+    return ans;
+  }
 }
