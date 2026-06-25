1b500a92f35d0c5c41be90c66a129315f0fbca00
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-06-25 18:31:57 +0530

    Time: 0 ms (100%), Space: 43.4 MB (42.22%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,24 +1,38 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+
+        // Get length of input array
         int n = digits.length;
 
-        int i = n - 1; // start from LSB like normal addition
+        // Start from the last digit (least significant digit)
+        int i = n - 1;
 
+        // Traverse from right to left
         while (i >= 0) {
+
+            // If current digit is less than 9
+            // simply increase it by 1 and return
             if (digits[i] < 9) {
                 digits[i]++;
-                //return digits;
                 return digits;
             }
 
-            // digit was 9
+            // If digit is 9, then after adding 1
+            // it becomes 0 and carry moves to left
             digits[i] = 0;
             i--;
         }
 
-        // carry reached beyond the most significant digit
+        // If all digits were 9
+        // Example: [9,9,9] -> [1,0,0,0]
+
+        // Create new array of size n + 1
         int[] result = new int[n + 1];
+
+        // Put 1 at first position
         result[0] = 1;
+
+        // Remaining positions are already 0 by default
         return result;
     }
 }
