f51cda6147eec9560d46bc8311fd45f39dfcb39f
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-07-03 19:19:09 +0530

    Time: 693 ms (5%), Space: 44.8 MB (98.06%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,33 +1,34 @@
 class Solution {
-    public int threeSumClosest(int[] nums, int target) {
+  public int threeSumClosest(int[] nums, int target) {
+   
+    int len = nums.length;
+    
+    int sum = 0;
 
-        int len = nums.length;
+    // First 3 elements ka sum initial closest answer maan liya
+    int closestSum = nums[0] + nums[1] + nums[2];
 
-        int sum = 0;
+    // First element choose kar rahe hain
+    for (int i = 0; i < len - 2; i++) {
 
-        // First 3 elements ka sum initial closest answer maan liya
-        int closestSum = nums[0] + nums[1] + nums[2];
+      // Second element choose kar rahe hain
+      for (int j = i + 1; j < len - 1; j++) {
 
-        // First element choose kar rahe hain
-        for (int i = 0; i < len - 2; i++) {
+        // Third element choose kar rahe hain
+        for (int k = j + 1; k < len; k++) {
 
-            // Second element choose kar rahe hain
-            for (int j = i + 1; j < len - 1; j++) {
+          // Current triplet ka sum
+          sum = nums[i] + nums[j] + nums[k];
 
-                // Third element choose kar rahe hain
-                for (int k = j + 1; k < len; k++) {
-
-                    // Current triplet ka sum
-                    sum = nums[i] + nums[j] + nums[k];
-
-                    // Agar current sum target ke aur paas hai
-                    if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
-                        closestSum = sum;
-                    }
-                }
-            }
+          // Agar current sum target ke aur paas hai
+          if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+            closestSum = sum;
+          }
+          
         }
-
-        return closestSum;
+      }
     }
+
+    return closestSum;
+  }
 }
