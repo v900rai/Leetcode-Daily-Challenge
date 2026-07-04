5795cfa8f090fe6234d3f062c6c09518450a2629
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-07-04 18:36:15 +0530

    Time: 4 ms (82.15%), Space: 46.5 MB (24.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,15 @@
 class Solution {
   public boolean isAnagram(String s, String t) {
-    
+
     if(s.length() != t.length()){
       return false;
     }
     char[] sarr = s.toCharArray();
     char[] tarr = t.toCharArray();
-    
+
     Arrays.sort(sarr);
     Arrays.sort(tarr);
-    return Arrays.equals(sarr ,tarr);
+    return Arrays.equals(sarr, tarr);
 
   }
 }
