a9c89362f75a48e7e02e503d94436283847c804c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-07-04 18:21:16 +0530

    Time: 7 ms (22.68%), Space: 42.8 MB (66.83%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -1,26 +1,26 @@
 class Solution {
-    public int maxScore(String s) {
-         int n = s.length();
-        int result = Integer.MIN_VALUE;
+  public int maxScore(String s) {
+    int n = s.length();
+    int result = Integer.MIN_VALUE;
 
-        for (int i = 0; i < n - 1; i++) {
-            int zerosLeft = 0;
-            for (int j = 0; j <= i; j++) {
-                if (s.charAt(j) == '0') {
-                    zerosLeft++;
-                }
-            }
-
-            int onesRight = 0;
-            for (int j = i + 1; j < n; j++) {
-                if (s.charAt(j) == '1') {
-                    onesRight++;
-                }
-            }
-
-            result = Math.max(result, zerosLeft + onesRight);
+    for (int i = 0; i < n - 1; i++) {
+      int zerosLeft = 0;
+      for (int j = 0; j <= i; j++) {
+        if (s.charAt(j) == '0') {
+          zerosLeft++;
         }
-        return result;
-    
+      }
+
+      int onesRight = 0;
+      for (int j = i + 1; j < n; j++) {
+        if (s.charAt(j) == '1') {
+          onesRight++;
+        }
+      }
+
+      result = Math.max(result, zerosLeft + onesRight);
     }
+    return result;
+
+  }
 }
