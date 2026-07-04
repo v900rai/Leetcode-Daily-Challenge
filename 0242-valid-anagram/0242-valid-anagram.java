4bf085ab7ec32e6bc512d89666160d720688eb79
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-07-04 18:28:44 +0530

    Time: 4 ms (82.15%), Space: 46.6 MB (24.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -6,6 +6,7 @@ class Solution {
     }
     char[] sarr = s.toCharArray();
     char[] tarr = t.toCharArray();
+    
     Arrays.sort(sarr);
     Arrays.sort(tarr);
     return Arrays.equals(sarr ,tarr);
