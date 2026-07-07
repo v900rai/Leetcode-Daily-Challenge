9cb07ad7f6af766688d9bfc1d0cedd99d8a7e650
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-07-07 10:26:04 +0530

    Time: 1 ms (99.85%), Space: 42.5 MB (67.71%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,21 +1,21 @@
 class Solution {
-    public long sumAndMultiply(int n) {
-        long num = 0;
-        long sum = 0;
-        long mul = 1;
-        
+  public long sumAndMultiply(int n) {
+    long num = 0;
+    long sum = 0;
+    long mul = 1;
 
-        while (n > 0) {
-            int digit = n % 10;
-            sum += digit;
+    while (n > 0) {
+      int digit = n % 10;
+     
+      sum = sum + digit;
 
-            if (digit > 0) {
-                num = (digit * mul) + num;
-                mul = mul * 10;
-            }
+      if (digit > 0) {
+        num = (digit * mul) + num;
+        mul = mul * 10;
+      }
 
-            n = n / 10;
-        }
-        return (sum * num);
+      n = n / 10;
     }
+    return (sum * num);
+  }
 }
