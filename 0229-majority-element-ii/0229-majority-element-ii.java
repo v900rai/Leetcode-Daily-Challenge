cc4be5844a93a2c16ef49801a70f193d116d07ae
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-07-12 13:29:32 +0530

    Time: 5 ms (51.24%), Space: 52.5 MB (52.86%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -11,14 +11,18 @@ public class Solution {
     for (int num : nums) {
       if (maj1 != null && num == maj1) {
         count1++;
+
       } else if (maj2 != null && num == maj2) {
         count2++;
+
       } else if (count1 == 0) {
         maj1 = num;
         count1 = 1;
+
       } else if (count2 == 0) {
         maj2 = num;
         count2 = 1;
+        
       } else {
         count1--;
         count2--;
