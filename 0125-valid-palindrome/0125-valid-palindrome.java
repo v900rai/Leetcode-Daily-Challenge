2f755e210c58fef0f9bf35d17c836c1ae9892fb0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-07-17 14:22:59 +0530

    Time: 2 ms (99.29%), Space: 44.3 MB (75.37%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,21 +2,21 @@
 class Solution {
   public boolean isPalindrome(String s) {
     int start = 0;
-    int end   = s.length()-1;
-    while(start <end){
-      while(start < end && !Character.isLetterOrDigit(s.charAt(start))){
+    int end = s.length() - 1;
+    while (start < end) {
+      while (start < end && !Character.isLetterOrDigit(s.charAt(start))) {
         start++;
       }
-      while(start < end  && !Character.isLetterOrDigit(s.charAt(end))){
+      while (start < end && !Character.isLetterOrDigit(s.charAt(end))) {
         end--;
       }
       char endchar = Character.toLowerCase(s.charAt(end));
       char startchar = Character.toLowerCase(s.charAt(start));
-      
+
       if (startchar != endchar) {
         return false;
       }
-       // Move both pointers towards the center
+      // Move both pointers towards the center
       start++;
       end--;
     }
@@ -24,5 +24,5 @@ class Solution {
     // If all characters matched, it's a palindrome
     return true;
 
-    }
+  }
 }
