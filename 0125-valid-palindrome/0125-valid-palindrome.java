0fd505e35c2f33fd07c6e4a34ed9e593321c28f6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-07-17 14:14:02 +0530

    Time: 2 ms (99.29%), Space: 44.5 MB (56.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,17 +4,12 @@ class Solution {
     int start = 0;
     int end   = s.length()-1;
     while(start <end){
-      
       while(start < end && !Character.isLetterOrDigit(s.charAt(start))){
         start++;
       }
-      
       while(start < end  && !Character.isLetterOrDigit(s.charAt(end))){
         end--;
       }
-
-      
-      
       char endchar = Character.toLowerCase(s.charAt(end));
       char startchar = Character.toLowerCase(s.charAt(start));
       
