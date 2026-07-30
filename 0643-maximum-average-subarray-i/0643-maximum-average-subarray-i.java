af55f34abab2273df8ed2dd2e47daea9ad46230a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-07-30 17:23:54 +0530

    Time: 2 ms (99.76%), Space: 69.4 MB (80.68%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,26 +1,38 @@
 class Solution {
-    public double findMaxAverage(int[] nums, int k) {
-     int sum = 0;
+  public double findMaxAverage(int[] nums, int k) {
+    int sum = 0;
 
-        // 🔹 first window sum
-        for (int i = 0; i < k; i++) {
-            sum += nums[i];
-        }
+    // 🔹 first window sum
+    for (int i = 0; i < k; i++) {
+      sum += nums[i];
+    }
+    // // first window sum 
+    // for (int i = 0; i < k; i++) {
+    //   sum = sum + nums[i];
+    // }
 
-        int maxSum = sum;
+    //int maxSum = sum;
+    int maxSum = sum;
 
-        // 🔹 slide the window
-        for (int i = k; i < nums.length; i++) {
+    // 🔹 slide the window
+    // for (int i = k; i < nums.length; i++) {
 
-            sum += nums[i];      // right se add
-            sum =sum- nums[i - k];  // left se remove
+    //   sum += nums[i]; // right se add
+    //   sum = sum - nums[i - k]; // left se remove
 
-            maxSum = Math.max(maxSum, sum);
-        }
-
-        // 🔹 average = maxSum / k
-        return (double) maxSum / k;
+    //   maxSum = Math.max(maxSum, sum);
+    // }
+    // slid the window 
+    for (int i = k; i < nums.length; i++) {
+      sum = sum + nums[i];
+      sum = sum - nums[i - k];
+      maxSum = Math.max(maxSum, sum);
     }
 
-    
+    // 🔹 average = maxSum / k
+    //return (double) maxSum / k;
+
+    return (double) maxSum /k;
+  }
+
 }
