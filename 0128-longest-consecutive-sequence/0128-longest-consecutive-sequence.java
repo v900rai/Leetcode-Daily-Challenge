f2f01d69c5b0f0344d9cae842bb977346cc33209
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-08-05 18:21:06 +0530

    Time: 31 ms (58.65%), Space: 95 MB (71.87%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,36 +1,35 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
+  public int longestConsecutive(int[] nums) {
 
-        // Step 1: Sabhi elements ko HashSet me daal do
-        // Taaki O(1) time me search ho sake 
-        
-        HashSet<Integer> set = new HashSet<>();
-        for(int val : nums){
-          set.add(val);
-        }
+    // Step 1: Sabhi elements ko HashSet me daal do
+    // Taaki O(1) time me search ho sake 
 
-
-        int max = 0; // Longest sequence ka answer
-        // Step 2: Har number ke liye check karo
-        for(int num : set){
-            // Agar num-1 present nahi hai,
-            // iska matlab yeh sequence ka starting point hai
-            if(!set.contains(num - 1)){
-
-                int count = 0;     // current sequence length
-                int curr = num;    // current number
-
-                // Step 3: Consecutive numbers count karo
-                while(set.contains(curr)){
-                    count++;
-                    curr++;
-                }
-
-                // Step 4: Max length update karo
-                max = Math.max(max, count);
-            }
-        }
-
-        return max;
+    HashSet<Integer> set = new HashSet<>();
+    for (int val : nums) {
+      set.add(val);
     }
+
+    int max = 0; // Longest sequence ka answer
+    // Step 2: Har number ke liye check karo
+    for (int num : set) {
+      // Agar num-1 present nahi hai,
+      // iska matlab yeh sequence ka starting point hai
+      if (!set.contains(num - 1)) {
+
+        int count = 0; // current sequence length
+        int curr = num; // current number
+
+        // Step 3: Consecutive numbers count karo
+        while (set.contains(curr)) {
+          count++;
+          curr++;
+        }
+
+        // Step 4: Max length update karo
+        max = Math.max(max, count);
+      }
+    }
+
+    return max;
+  }
 }
