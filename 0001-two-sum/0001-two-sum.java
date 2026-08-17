16fb9c2520a27963320d98de2aa25c471f03666c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-08-17 12:50:22 +0530

    Time: 2 ms (99.34%), Space: 47.4 MB (11.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,9 +5,10 @@ class Solution {
     //Map<Integer, Integer> map = new HashMap<>();
     Map<Integer, Integer> map = new HashMap<>();
     for (int i = 0; i < nums.length; i++) {
-      int diff = target -nums[i];
-      if(map.containsKey(diff)){
+      //int diff = target -nums[i];
       
+      int diff = target - nums[i];
+      if(map.containsKey(diff)){
         return new int[] {map.get(diff),i};
       }
       map.put(nums[i], i);
